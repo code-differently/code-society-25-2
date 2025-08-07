a42d28e119d14f62e9ac82307862a43c0a13454a
Tyran Rice <tyranricejr@gmail.com>
2025-08-07 13:55:14 +0000

    refactor: changes Tyran's markdown to html prototype to fully developed and working

--- lesson_01/tyranricejr/markdownparser.py
+++ lesson_01/tyranricejr/markdownparser.py
@@ -7,8 +7,25 @@ def main():
             markdown_content = md_file.read()
 
         html_content = markdown.markdown(markdown_content)
-        print("\nConverted HTML content:")
-        print(html_content)
+        print("\nConverted HTML content")
+
+        with open("./htmlmarkdown", "w") as f:
+            f.write("<!DOCTYPE html>\n")
+            f.write("<html lang='en'>\n")
+            f.write("""<head>
+                <meta charset = "UTF-8" >
+                <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.2.0/github-markdown-light.min.css" >
+                <style >
+                body {
+                    max-width: 800px;
+                    margin: auto;
+                    padding: 2rem;
+                }
+                </style >
+            </head >
+            <body class = "markdown-body" >""")
+            f.write(html_content + "\n")
+            f.write("</html>")
 
     except FileNotFoundError:
         print("Error: The specified Markdown file was not found.")
