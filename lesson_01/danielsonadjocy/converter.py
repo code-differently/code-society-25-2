3c00d259205c6cf60581fdc323dd4c9f593ecf5b
Danielson A <adjocydanielson@gmail.com>
2025-08-07 10:53:46 -0400

    refactor: Removed Path import

--- lesson_01/danielsonadjocy/converter.py
+++ lesson_01/danielsonadjocy/converter.py
@@ -1,10 +1,13 @@
 import markdown
 import sys
-from pathlib import Path
 import re
 
 def convert_md_to_html(markdown_file, html_file):
-    md = Path(markdown_file).read_text(encoding='utf-8')
+    f = open(markdown_file, "r", encoding="utf-8")
+    md = f.read()
+
+    write = open(html_file, "w", encoding="utf-8")
+
 
     md = re.sub(r"([^\n])\n([*+-] )", r'\1\n\n\2', md)
     md = re.sub(
@@ -12,14 +15,14 @@ def convert_md_to_html(markdown_file, html_file):
         r'[\1](\1)',
         md
     )
-
+    
     html = markdown.markdown(md, extensions=['fenced_code', 'codehilite'])
 
     full_html = f"""<!DOCTYPE html>
     <html lang="en">
     <head>
     <meta charset="UTF-8">
-    <title>{Path(markdown_file).stem}html</title>
+    <title>{markdown_file[:-3]} html</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/github-markdown-css@5/github-markdown.min.css">    
     </head>
@@ -29,12 +32,14 @@ def convert_md_to_html(markdown_file, html_file):
     </html>
     """
 
-    Path(html_file).write_text(full_html, encoding='utf-8')
+    write.write(full_html)
+    f.close()
+    write.close()
 
 if __name__ == "__main__":
     try:
         readme_md = sys.argv[1]
-        readme_html = sys.argv[2] if len(sys.argv) > 2 else "README.html"
+        readme_html = sys.argv[2] if len(sys.argv) > 2 else readme_md[:-3]+ ".html"
         print(readme_html)
         convert_md_to_html(readme_md, readme_html)
     except Exception as e:
