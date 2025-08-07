a33124932152741a732c0732142bbcc53f4dd690
Tyran Rice <tyranricejr@gmail.com>
2025-08-07 16:04:41 +0000

    chore: adds Tyran's README file for the markdownparser and additional fixes to the code

---
+++ lesson_01/tyranricejr/README.md
@@ -0,0 +1,8 @@
+# How to use Markdown Parser
+
+1. Using the terminal, get into the local folder of 'markdownparser.py'
+1. When inside the terminal, type 'python markdownparser.py'
+1. The program will ask you to enter your file path of the mark down file you want to convert
+1. Once you input the file, the html file will be created in the same folder as the 'markdownparser.py'
+
+* Created By Tyran Rice Jr. *
--- lesson_01/tyranricejr/markdownparser.py
+++ lesson_01/tyranricejr/markdownparser.py
@@ -3,19 +3,29 @@ import markdown
 
 def main():
     try:
-        with open("../README.md", "r", encoding="utf-8") as md_file:
-            markdown_content = md_file.read()
+        print("Welcome to Tyran's Markdown-To-HTML Converter")
+        markdown_file = input("Please enter the path of the mark down file: ")
 
+        with open(markdown_file, "r", encoding="utf-8") as md_file:
+            markdown_content = md_file.readlines()
+
+        markdown_file = markdown_file.removesuffix("README.md")
+        for i in range(len(markdown_content)):
+            if (markdown_content[i].find("src=") != -1):
+                index = markdown_content[i].find("src=")
+                markdown_content[i] = markdown_content[i][:index + 4] + \
+                    "\"" + markdown_file + markdown_content[i][index + 5:]
+
+        markdown_content = ''.join(markdown_content)
         html_content = markdown.markdown(markdown_content)
-        print("\nConverted HTML content")
 
-        with open("./htmlmarkdown", "w") as f:
+        with open("./htmlmarkdown.html", "w") as f:
             f.write("<!DOCTYPE html>\n")
             f.write("<html lang='en'>\n")
             f.write("""<head>
-                <meta charset = "UTF-8" >
-                <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.2.0/github-markdown-light.min.css" >
-                <style >
+            <meta charset = "UTF-8" >
+            <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.2.0/github-markdown-light.min.css" >
+            <style >
                 body {
                     max-width: 800px;
                     margin: auto;
@@ -27,10 +37,12 @@ def main():
             f.write(html_content + "\n")
             f.write("</html>")
 
+        print("Converted Markdown To HTML content")
+
     except FileNotFoundError:
         print("Error: The specified Markdown file was not found.")
     except Exception as e:
-        print(f"An error occurred during Markdown processing: {e}")
+        print(f"An error occurred!\nError: {e}")
 
 
 if __name__ == "__main__":
