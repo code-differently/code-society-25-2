0cc4fd0f85e74936ccf8ed189b9a5c9d9e5f78c2
Daniel Boyce <danielboyce@Daniels-MacBook-Air.local>
2025-08-07 11:07:37 -0400

    chore: added css to match the readme from browser dev tools

--- lesson_01/danielboyce/strech.py
+++ lesson_01/danielboyce/strech.py
@@ -3,6 +3,7 @@ import markdown2
 HTML_TEMPLATE = """<!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="styles.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{title}</title>
