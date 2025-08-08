e308ae4f8c6ed75debcbfbc0b89714ce8699b4ef
Danielson A <adjocydanielson@gmail.com>
2025-08-08 01:06:09 -0400

    feat: Working with multiple files; creates zip

--- lesson_01/danielsonadjocy/converter_flask.py
+++ lesson_01/danielsonadjocy/converter_flask.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, send_file
 from flask_cors import CORS
 import markdown
 import sys
 import re
 from io import BytesIO
+import zipfile
+
 
 app = Flask(__name__)
 CORS(app)
@@ -44,25 +46,53 @@ def convert_md_to_html(filename, md):
 
 @app.route("/convert", methods = ["POST"])
 def convert_file():
-
     if "markdown" not in request.files:
-        return jsonify({"error": "No file or file not a markdown."}), 400
+        return jsonify({"error": "No markdown files"}), 400
+    
+    markdown_files = request.files.getlist("markdown")
+    others = request.files.getlist("files")
 
-    file = request.files["markdown"]
-    filename = file.filename[:-3]
-    readme_md = file.read().decode("utf-8")
+    # One File
+    if len(markdown_files) == 1 and len(others) == 0:
+        file = markdown_files[0]
+        filename = file.filename[:-3]
+        readme_md = file.read().decode("utf-8")
+        
+        try:
+            print(filename)
+            readme_html = convert_md_to_html(filename, readme_md)
+            response = make_response(readme_html)
+            response.headers["Content-Type"] = "text/html"
+            response.headers["Content-Disposition"] = f"attachment; filename={filename}.html"
+            return response
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
+    #Multiple files
+    else:
+        zip_buffer = BytesIO()
+        with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zip_file:
+            for md_file in markdown_files:
+                filename = md_file.filename[:-3]
+                readme_md = md_file.read().decode("utf-8")
+                readme_html = convert_md_to_html(filename, readme_md)
+                zip_file.writestr(f"{filename}.html", readme_html)
+                print(filename)
+            
+            for other_file in others:
+                # For images to put them in images folder
+                if other_file.filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.svg')):
+                    zip_file.writestr(f"images/{other_file.filename}", other_file.read())
+                else:
+                    zip_file.writestr(other_file.filename, other_file.read())
+                print(other_file.filename)
 
-    try:
-        print(filename)
-        readme_html = convert_md_to_html(filename, readme_md)
-        response = make_response(readme_html)
-        ##Might not be necessary
-        response.headers["Content-Type"] = "text/html"
-        response.headers["Content-Disposition"] = f"attachment; filename={filename}.html"
-        #################################################################################
-        return response
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        zip_buffer.seek(0)
+        return send_file(
+            zip_buffer,
+            mimetype="application/zip",
+            as_attachment=True,
+            download_name="converted_files.zip"
+        )
     
 
 
--- lesson_01/danielsonadjocy/md_converter/src/components/Dragdrop.jsx
+++ lesson_01/danielsonadjocy/md_converter/src/components/Dragdrop.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 import "./Dragdrop.css";
-import { FaUpload, FaDownload} from 'react-icons/fa';
+import { FaUpload, FaDownload, FaFileAlt} from 'react-icons/fa';
 
 
 
@@ -10,11 +10,11 @@ const DragDrop = props => {
 
     const wrapperRef = useRef(null);
 
-    const [file, setFile] = useState(null);
+    const [fileList, setFileList] = useState([]);
 
-    const [fileDropped, setFileDropped] = useState(false);
+    const [fileDropped, setFileDropped] = useState(false);// Could be replaced by checking the list length
     
-    const [fileName,setFileName] = useState("");
+    //const [fileName,setFileName] = useState(""); //u
 
     const [downloadURL, setDownloadURL] = useState(null)
 
@@ -23,7 +23,7 @@ const DragDrop = props => {
     const onDrop = () => wrapperRef.current.classList.remove('dragover');
 
     const onFileDrop = (e) => {
-        const newFile = e.target.files[0];
+        /*const newFile = e.target.files[0];
         if (newFile) {
             setFileDropped(true);
             setFile(newFile);
@@ -31,10 +31,25 @@ const DragDrop = props => {
             
             handleFileUpload(newFile);
             //props.onFileChange([newFile]); Should need this later
+        }*/
+        const newFiles = Array.from(e.target.files);
+
+        const filtered = newFiles.filter(
+            f => !fileList.some(existing => existing.name === f.name)
+        );
+        if (filtered.length == 0){
+            return;
         }
+        setFileList(prev => [...prev, ...filtered]);
     }
 
-    const handleFileUpload = async (file) => {
+    const removeFile = (file) => {
+        const updatedList = [...fileList];
+        updatedList.splice(fileList.indexOf(file), 1);
+        setFileList(updatedList);
+    }
+
+    /*const handleFileUpload = async (file) => {
         if (!file){
             return;
         }
@@ -66,27 +81,54 @@ const DragDrop = props => {
         }
 
 
-    }
+    }*/
 
-    // TODO: add download function
-    const handleDownload = () =>{
-        if (!fileName){
+    const handleDownload = async () =>{
+        /*if (!fileName){
             return;
         }
         const a = document.createElement("a");
         a.href = downloadURL;
         a.download = fileName;
-        a.click();
+        a.click();*/
+        if (fileList.length == 0){
+            return;
+        }
+        const formData = new FormData();
+        fileList.forEach(file => {
+            if (file.name.toLowerCase().endsWith(".md")){
+                formData.append("markdown", file);
+            }
+            else{
+                formData.append("files", file)
+            }
+        });
+        try {
+            const response = await fetch('http://localhost:5001/convert', {// REMEMBER THIS
+            method: "POST",
+            body: formData
+            });
+
+            if (!response.ok){
+                throw new Error("Conversion failed");
+            }
+            
+            console.log("Sending succeed!!")// to be deleted
+
+            const blob = await response.blob();
+            
+            const url = window.URL.createObjectURL(blob);
+
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = fileList.length === 1 ? fileList[0].name.replace(/\.md$/, ".html") : "converted_files.zip";
+            a.click();
+
+        } catch (err) {
+            console.error(err)
+        }
     }
 
-
-    /*
-    const fileRemove = (file) => {
-        const updatedList = [...fileList];
-        updatedList.splice(fileList.indexOf(file), 1);
-        setFileList(updatedList);
-        props.onFileChange(updatedList);
-    }*/
 
     return (
         <>
@@ -101,31 +143,38 @@ const DragDrop = props => {
                     <FaUpload style={{ fontSize: '10rem'}}/>
                     <p>Drag & Drop your files here</p>
                 </div>
-                <input type="file" value="" onChange={onFileDrop} className='upload-download'/>
+                <input type="file" multiple onChange={onFileDrop} className='upload-download'/>
             </div>
 
-            {/*Download button*/
-                fileDropped &&
-            <div
-                ref={wrapperRef}
-                className="drop-file-input"
-                onDragEnter={onDragEnter}
-                onDragLeave={onDragLeave}
-                onDrop={onDrop}
-            >
-                <div className="drop-file-input__label">
-                    <FaDownload style={{ fontSize: '10rem'}}/>
-                    <p>{fileName}</p>
-                </div>
-                <button className='upload-download' onClick={handleDownload}></button>
-            </div>
-            }
+            {
+                fileList.length > 0 && (
+
+                <div className="drop-file-preview">
+                    <p className="drop-file-preview__title" >Files ready:</p>
+                    {fileList.map(file => (
+                        <div key={`${file.name}`} className="drop-file-preview__item">
+                        <FaFileAlt/>
+                        <div className="drop-file-preview__item__info">{file.name}</div>
+                        <span className="drop-file-preview__item__del" onClick={() => removeFile(file)}>x</span>
+                        </div>
+                    ))}
+                    <div
+                        ref={wrapperRef}
+                        className="drop-file-input"
+                        onDragEnter={onDragEnter}
+                        onDragLeave={onDragLeave}
+                        onDrop={onDrop}
+                    >
+                        <div className="drop-file-input__label">
+                            <FaDownload style={{ fontSize: '10rem'}}/>
+                            <p>Download file(s) (line 167)</p>
+                        </div>
+                        <button className='upload-download' onClick={handleDownload}></button>
+                    </div>
+                </div>       
+            )}
         </>
     );
-}
-
-DragDrop.propTypes = {
-    onFileChange: PropTypes.func
 }
 
 export default DragDrop;
