a44094c4cfd837ea387cc586d0785c3f2ca18b38
Daniel Boyce <danielboyce@Daniels-MacBook-Air.local>
2025-08-08 15:56:43 -0400

    fix:added some comments and spacing to make code more readable

--- lesson_01/danielboyce/md_to_html_converter.py
+++ lesson_01/danielboyce/md_to_html_converter.py
@@ -13,24 +13,39 @@ HTML_TEMPLATE = """<!DOCTYPE html>
 </body>
 </html>
 """
+
 def extract_title(html_content):
+
     start_tag = "<h1>"
     end_tag = "</h1>"
     start_index = html_content.find(start_tag)
     end_index = html_content.find(end_tag)
+    # uses string method to find title between h1 tags 
+
     if start_index != -1 and end_index != -1:
         return html_content[start_index + len(start_tag):end_index].strip()
+    # if no h1 tags found, return default title
     return "Document"
+
 def convert_to_html(file_name):
+
     with open(file_name,"r",encoding="utf-8") as f:
         md_text = f.read()
+        # read markdown file content
     html_output = markdown2.markdown(md_text, extras=["fenced-code-blocks", "tables", "code-friendly"])
+    # convert markdown to html
     title = extract_title(html_output)
+    # call function to extract title
     full_html = HTML_TEMPLATE.format(title=title, content=html_output)
+    # format html template with title and content
     output_path = file_name.rsplit('.', 1)[0] + '.html'
+    # create new file name with .html extension
     with open(output_path, "w", encoding="utf-8") as f:
         f.write(full_html)
+        #write new html content to file
     return output_path
+
+
 
 if __name__ == "__main__":
     convert_to_html(sys.argv[1])
