63128c5281962ceff1f86dffbc38673e7c461c40
Talia Crockett <tmcrockett252@gmail.com>
2025-08-12 15:29:52 +0000

    feat(quiz): create custom quiz in TypeScript and update quiz.yaml & quizzes.module.ts

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
 import { BrooklynHardenQuiz } from './brooklyn_harden_quiz.js';
+import { TaliaCrockettQuiz } from './talia_crockett_quiz.js';
 export const Quizzes = Symbol.for('Quizzes');
 
 // Add your quiz provider here.
 
-const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz, BrooklynHardenQuiz];
+const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz, BrooklynHardenQuiz, TaliaCrockettQuiz];
 
 @Module({
   providers: [
