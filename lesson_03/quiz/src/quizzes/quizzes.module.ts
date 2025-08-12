80ce768f1f4f1e467fde2a7d207a89712bd2eda6
jmckellar252-png <jmckellar252@gmail.com>
2025-08-12 15:40:15 +0000

    test: ensure quiz passes all checks

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
+import { JoneemckellarQuiz } from './joneemckellar_quiz.js';
+
 export const Quizzes = Symbol.for('Quizzes');
 
 // Add your quiz provider here.
 
 const QUIZ_PROVIDERS = [
-  AnthonyMaysQuiz,
-  AnotherQuiz,
-
+  AnthonyMaysQuiz, 
+  AnotherQuiz, 
+  JoneemckellarQuiz,
 ];
 
 @Module({
