17731cec05682451c6ecc8f382e22697cf66b1c8
Daniel Boyce <danielboyce20@gmail.com>
2025-08-13 18:47:47 +0000

    chore:fixes junit imports to j unit 5

--- lesson_04/danielboyce/java_is_prime/src/test/java/IsPrimeTest.java
+++ lesson_04/danielboyce/java_is_prime/src/test/java/IsPrimeTest.java
@@ -1,3 +1,16 @@
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import com.primenumbers.IsPrime;
+
 public class IsPrimeTest {
+    
+
+    @Test
+    public void testIsPrime() {
+        assertTrue(IsPrime.isPrime(2));
+    }
+
     
 }
