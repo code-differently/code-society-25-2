0c5e68621712e8817dfe34a1445fafa6181515e4
tobyev <teevans252@gmail.com>
2025-08-14 00:33:37 +0000

    Restore all lesson_03 quiz files

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
-import { TobaisEvansQuiz } from './tobais_evans_quiz.js';
 export const Quizzes = Symbol.for('Quizzes');
 
 // Add your quiz provider here.
 
-const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz, TobaisEvansQuiz];
+const QUIZ_PROVIDERS = [
+  AnthonyMaysQuiz,
+  AnotherQuiz,
+
+];
 
 @Module({
   providers: [
