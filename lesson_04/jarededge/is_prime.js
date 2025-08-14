f644673d1d3dfe6bb8b63d345f6a57dd7efb264e
Jared Edge <jarededgewrk@gmail.com>
2025-08-14 19:40:14 +0000

    feat:adds reafactored .py, .js, .ts files to reduce the logic per function

--- lesson_04/jarededge/is_prime.js
+++ lesson_04/jarededge/is_prime.js
@@ -1,23 +1,35 @@
-function isPrime(n) {
-    if ( n <= 1) return false;
-    if(n<=3) return true;
-    if(n % 2 === 0 || n % 3 === 0) return false;
+function isLessThanOrEqualToOne(n) {
+    return n <= 1;
+}
 
+function isTwoOrThree(n) {
+    return n === 2 || n === 3;
+}
+
+function isDivisibleByTwoOrThree(n) {
+    return n % 2 === 0 || n % 3 === 0;
+}
+
+function hasOtherDivisors(n) {
     let i = 5;
-    while(i * i <= n) {
+    while (i * i <= n) {
         if (n % i === 0 || n % (i + 2) === 0) {
-            return false;
-
-        
+            return true;
+        }
+        i += 6;
     }
-i += 6;
-}
-return true;
+    return false;
 }
 
-//Test the program before JUnit Tests
+function isPrime(n) {
+    if (isLessThanOrEqualToOne(n)) return false;
+    if (isTwoOrThree(n)) return true;
+    if (isDivisibleByTwoOrThree(n)) return false;
+    return !hasOtherDivisors(n);
+}
+
+// Testing before JUnit tests
 const testNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 16, 17, 19, 21, 23, 26];
 testNumbers.forEach((num) => {
     console.log(`${num} is prime? ${isPrime(num)}`);
-
 });
--- lesson_04/jarededge/is_prime.ts
+++ lesson_04/jarededge/is_prime.ts
@@ -1,24 +1,36 @@
-function isPrimeTS(n: number): boolean{
-    if (n <= 1) return false;
-    if (n<= 3 ) return true;
-    if (n % 2 === 0 || n % 3 === 0) return false;
+function isLessThanOrEqualToOneTS(n: number): boolean {
+    return n <= 1;
+}
 
+function isTwoOrThreeTS(n: number): boolean {
+    return n === 2 || n === 3;
+}
+
+function isDivisibleByTwoOrThreeTS(n: number): boolean {
+    return n % 2 === 0 || n % 3 === 0;
+}
+
+function hasOtherDivisorsTS(n: number): boolean {
     let i = 5;
-    while (i * i <=n) {
-        if (n % i === 0 || n % (i + 2) === 0){
+    while (i * i <= n) {
+        if (n % i === 0 || n % (i + 2) === 0) {
             return true;
-
         }
         i += 6;
     }
     return false;
 }
 
-//Testing the program before JUNIT tests
+function isPrimeTS(n: number): boolean {
+    if (isLessThanOrEqualToOne(n)) return false;
+    if (isTwoOrThree(n)) return true;
+    if (isDivisibleByTwoOrThree(n)) return false;
+    return !hasOtherDivisors(n);
+}
 
+// Testing before JUnit tests
 const testNumbersTS: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 16, 17, 19, 21, 23, 26];
 
 testNumbersTS.forEach((num) => {
-    console.log(`${num} is prime? ${isPrime(num)}`);
-
+    console.log(`${num} is prime? ${isPrimeTS(num)}`);
 });
