2550e7022fa7e6dc188b85fc6d54a65abd4b2f58
Danielson A. <adjocydanielson@gmail.com>
2025-08-14 20:21:25 +0000

    README updated again

--- lesson_04/danielsonadjocy/app/src/main/python/prime.py
+++ lesson_04/danielsonadjocy/app/src/main/python/prime.py
@@ -1,4 +1,13 @@
 def is_prime(num):
+    """
+    Checks if num is a prime number.
+
+    Args:
+        num (int): The number to check.
+
+    Returns:
+        bool: True if num is prime, False otherwise.
+    """
     if num <= 1:
         return False
     for val in range(2,int(num**(1/2))+1):
@@ -9,5 +18,6 @@ def is_prime(num):
 if __name__ == "__main__":
     print(4, is_prime(4))
     print(13, is_prime(13))
+    # Checks each number between 2 and 100 to see if they're prime.
     for num in range(2, 101):
         print(str(num) +": "+ str(is_prime(num)))
