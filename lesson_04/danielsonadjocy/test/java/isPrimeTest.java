ac73b1303f3262b19a1eef603ec8eeeb86d387ed
Danielson A. <adjocydanielson@gmail.com>
2025-08-14 14:03:56 +0000

    feat:java test started

---
+++ lesson_04/danielsonadjocy/test/java/isPrimeTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class isPrimeTest {
+
+    @Test
+    void testPrime(){
+        assertTrue(Prime.isPrime(7));
+    }
+
+}
