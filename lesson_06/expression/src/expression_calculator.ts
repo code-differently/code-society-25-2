088bcb8e805abd0c977bd71f0ec64ed45b77870f
Wayleom Vargas Rubio <wayleomvr@Wayleoms-MacBook-Air.local>
2025-08-18 16:33:45 -0400

    Feat: calculator equation added - Wayleom Vargas

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,7 +2,8 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+    // Function to implement: Math.pow((a + b) / c, d) * e
+    return Math.pow((a + b) / c, d) * e;
   }
 
   pow(base: number, exponent: number): number {
