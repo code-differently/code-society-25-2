e66e95cffe0f03fbe927019ff981f3fbab68c2e3
Danielson A. <adjocydanielson@gmail.com>
2025-08-18 19:28:12 +0000

    feat: expressions added;functions working

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -1,10 +1,24 @@
 export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
-  calculate(a: number, b: number, c: number, d: number, e: number): number {
-    // Implement your code here to return the correct value.
-    return 0;
+  calculate(a: number, b: number, c: number, d: number, e: number): number { 
+    return this.multiply(this.pow(this.divide(this.add(a,b), c), d),e);
   }
 
+  add(a: number, b: number): number {
+    return a + b;
+  }
+  subtract(a: number, b: number): number {
+    return a - b;
+  }
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+  divide(a: number, b: number): number {
+    if (b === 0) {
+      throw new Error("Division by zero is not allowed.");
+    }
+    return a / b;
+  }
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
   }
