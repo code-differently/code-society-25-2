de1a7083a6db9c64c490114e63d8d09ebe0de12a
brooklynharden <hardenbrooklyn123@gmail.com>
2025-08-18 15:01:56 -0400

    feat: created my branch and put in my version for the assignment

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,6 +2,7 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
+
     return 0;
   }
 
