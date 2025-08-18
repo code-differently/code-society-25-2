df1eb21bd62f44f7c4127fc99ad30b036a602003
Daniel Boyce <danielboyce@Daniels-MacBook-Air.local>
2025-08-18 15:02:04 -0400

    finished expression calculator

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -1,8 +1,10 @@
 export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
-    // Implement your code here to return the correct value.
-    return 0;
+    let intial_value:number = a*b;
+    let second_value:number = this.pow(c+d,e);
+    
+    return intial_value/second_value;
   }
 
   pow(base: number, exponent: number): number {
