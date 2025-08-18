305f7d80202f869a19a3f2a7d445996a64deb9b6
Jared Edge <jarededgewrk@gmail.com>
2025-08-18 19:34:37 +0000

    feat: adds correct expressions into expression_calculator.ts

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,7 +2,13 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+    const sum = c + d;
+
+    const denominator = this.pow(sum, e);
+
+    const numerator = a * b;
+
+    return numerator / denominator;
   }
 
   pow(base: number, exponent: number): number {
