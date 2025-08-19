ba43a51dd3afae08eda8c566305f0bb607ab997e
Martha <iofuani252@gmail.com>
2025-08-19 15:46:47 +0000

    feat: add Martha's working expression calculator

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,10 +2,22 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+    return this.add(this.divide(a, b), this.multiply(c, this.pow(d, e)));
   }
 
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
+  }
+
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+
+  add(a: number, b: number): number {
+    return a + b;
+  }
+
+  divide(a: number, b: number): number {
+    return a / b;
   }
 }
