6054c74a31481afb255cf13f95c5a20b7b175232
NcoleJac <ndjackson@gmail.com>
2025-08-20 20:40:01 +0000

    feat: lesson06 modified files .env.test and expression calculator

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,7 +2,28 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+
+    const sum = this.add(a, b);
+
+    const quotient = this.divide(sum, c);
+
+    const powerResult = this.pow(quotient, d);
+
+    const product = this.multiply(powerResult, e);
+
+    return product;
+  }
+
+  add(a: number, b: number): number {
+    return a + b;
+  }
+
+  divide(a: number, b: number): number {
+    return a / b;
+  }
+
+  multiply(a: number, b: number): number {
+    return a * b;
   }
 
   pow(base: number, exponent: number): number {
