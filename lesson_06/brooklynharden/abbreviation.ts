2618b7929a6cc067756f61c5f020f0da4b053f47
brooklynharden <hardenbrooklyn123@gmail.com>
2025-08-20 09:45:59 -0400

    docs:changed parameter name and gthe format of comments

--- lesson_06/brooklynharden/abbreviation.ts
+++ lesson_06/brooklynharden/abbreviation.ts
@@ -1,13 +1,13 @@
 
-function isValidAlphaAbbreviation(word:string, abbr:string):boolean{
+function isValidAlphaAbbreviation(word:string, abbreviation:string):boolean{
     let i:number = 0; //keep the index of the word
-    for(let j=0; j<abbr.length;j++){
-        const abbrChar=abbr[j]; 
+    for(let j=0; j<abbreviation.length;j++){
+        const abbrChar=abbreviation[j]; 
 
-         /* We add 1 to shift the range, 'a'.charCodeAt(0) - 'a'.charCodeAt(0) = 0 ,
-            because 97-97 = 0. 
-            Adding the '1' shifts it to a===1, which is what we want.
-         */
+         // We add 1 to shift the range, 'a'.charCodeAt(0) - 'a'.charCodeAt(0) = 0 ,
+         // because 97-97 = 0. 
+         // Adding the '1' shifts it to a===1, which is what we want.
+        
         const skip =abbrChar.charCodeAt(0) - 'a'.charCodeAt(0) + 1 ; 
         if(word[i] === abbrChar){ 
             i++; //if the current word letter and the current abbrchar match match go to the next letter
