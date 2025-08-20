5c0e3594e95ded1efe126b6ca6d99b0356571517
Anthony D. Mays <anthony@morganlatimer.com>
2025-08-20 18:02:46 +0000

    chore: removing lesson_02 stuff
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- lesson_02/quiz_java/settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
-}
-
-rootProject.name = "lesson2-quiz-app"
-
-// Include the instructional library
-includeBuild("../../lib/java/codedifferently-instructional")
