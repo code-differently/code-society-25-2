f636ea0fee0c0bc8da1c81945d2fe93ecd179fca
jmckellar252-png <jmckellar252@gmail.com>
2025-08-21 13:18:58 +0000

    feat: Add function to determine letter graderefactor: Improve getLetterGrade logic with explicit ranges

--- lesson_07/conditionals/src/part_f.ts
+++ lesson_07/conditionals/src/part_f.ts
@@ -6,8 +6,18 @@
  * @returns
  */
 export function getLetterGrade(score: number): string {
-  return "";
-}
+   if (score >= 90 && score <= 100) {
+    return "A";
+  } else if (score >= 80 && score <= 89) {
+    return "B";
+  } else if (score >= 70 && score <= 79) {
+    return "C";
+  } else if (score >= 60 && score <= 69) {
+    return "D";
+  } else if (score < 60) {
+    return "F";
+  } else {
+
 
 /**
  * Calculates the sum of all even numbers in the given array.
