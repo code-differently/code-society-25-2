80d9430fad2e8759583eed61980b0c3a1e8be230
Danielson A. <adjocydanielson@gmail.com>
2025-08-25 20:39:12 +0000

    feat:provider finished

---
+++ lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/DanielsonAdjocyProvider.java
@@ -0,0 +1,22 @@
+package com.codedifferently.lesson9.dataprovider;
+
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DanielsonAdjocyProvider extends DataProvider {
+  public String getProviderName() {
+    return "danielsonadjocy";
+  }
+
+  public Map<String, Class> getColumnTypeByName() {
+    return Map.of(
+        "column1", Integer.class,
+        "column2", Short.class,
+        "column3", Long.class,
+        "column4", Float.class,
+        "column5", Boolean.class,
+        "column6", String.class,
+        "column7", Double.class);
+  }
+}
