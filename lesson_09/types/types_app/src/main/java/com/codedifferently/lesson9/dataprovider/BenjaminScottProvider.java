24494475c2663a28ad7289d8ef52a78407616c88
bscott519 <bbscott252@gmail.com>
2025-08-25 20:52:44 +0000

    working on provider java file

---
+++ lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/BenjaminScottProvider.java
@@ -0,0 +1,22 @@
+package com.codedifferently.lesson9.dataprovider;
+
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BenjaminScottProvider extends DataProvider {
+  public String getProviderName() {
+    return "benjaminscottprovider";
+  }
+
+  public Map<String, Class> getColumnTypeByName() {
+    return Map.of(
+        "column1", String.class,
+        "column2", Short.class,
+        "column3", Integer.class,
+        "column4", Boolean.class,
+        "column5", Double.class,
+        "column6", Float.class,
+        "column7", Long.class);
+  }
+}
