e4e83f5ed5a8e0fffb9d5170f3bc54f9cd61b07d
jmckellar252-png <jmckellar252@gmail.com>
2025-08-26 15:35:38 +0000

    feat: Add DataType enum for type mapping

---
+++ lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/model/DataType.java
@@ -0,0 +1,21 @@
+package com.codedifferently.lesson9.model;
+
+public enum DataType {
+    STRING(String.class),
+    INTEGER(Integer.class),
+    BOOLEAN(Boolean.class),
+    FLOAT(Float.class),
+    DOUBLE(Double.class),
+    LONG(Long.class),
+    SHORT(Short.class);
+
+    private final Class<?> javaType;
+
+    DataType(Class<?> javaType) {
+        this.javaType = javaType;
+    }
+
+    public Class<?> getJavaType() {
+        return javaType;
+    }
+}
