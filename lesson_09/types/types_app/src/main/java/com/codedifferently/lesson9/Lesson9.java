886212c4732c10fdf8b595975a4626e8e80a0070
bscott519 <bbscott252@gmail.com>
2025-08-26 16:41:36 +0000

    completed stretch assignment

--- lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/Lesson9.java
+++ lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/Lesson9.java
@@ -1,12 +1,17 @@
 package com.codedifferently.lesson9;
 
-import com.codedifferently.lesson9.generator.SampleFileGenerator;
 import java.io.File;
 import java.nio.file.Paths;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+
+import com.codedifferently.lesson9.dataprovider.DataProvider;
+import com.codedifferently.lesson9.generator.SampleFileGenerator;
 
 @Configuration
 @SpringBootApplication(scanBasePackages = "com.codedifferently")
@@ -17,19 +22,32 @@ public class Lesson9 implements CommandLineRunner {
     application.run(args);
   }
 
+  @Autowired
+  private List<DataProvider> dataProviders;
+
+  @Override
   public void run(String... args) throws Exception {
     if (args.length == 0) {
       return;
     }
 
-    var providerName = args[0];
-    if (providerName == null) {
-      throw new IllegalArgumentException("Provider name is required");
-    }
-
     String path = getDataPath();
     var fileGenerator = new SampleFileGenerator();
-    fileGenerator.createTestFile(path, providerName);
+
+    if (args[0].equals("--bulk")) {
+      // Bulk generation mode - generate for all providers
+      for (DataProvider provider : dataProviders) {
+        fileGenerator.createTestFile(path, provider.getProviderName());
+        System.out.println("Generated file for provider: " + provider.getProviderName());
+      }
+    } else {
+      // Single provider mode
+      var providerName = args[0];
+      if (providerName == null) {
+        throw new IllegalArgumentException("Provider name is required");
+      }
+      fileGenerator.createTestFile(path, providerName);
+    }
   }
 
   private static String getDataPath() {
