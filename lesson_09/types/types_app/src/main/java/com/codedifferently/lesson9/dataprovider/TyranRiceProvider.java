07023fe83ac5a9ebad177e66d3f28a4211b262fc
Tyran Rice <tyranricejr@gmail.com>
2025-08-26 15:16:16 +0000

    feat: adds Tyran's provider and JSON file

---
+++ lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/TyranRiceProvider.java
@@ -0,0 +1,24 @@
+package com.codedifferently.lesson9.dataprovider;
+
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TyranRiceProvider extends DataProvider {
+  public String getProviderName() {
+    return "tyranricejr";
+  }
+
+  public Map<String, Class> getColumnTypeByName() {
+    return Map.of(
+        "column1", Long.class,
+        "column2", Float.class,
+        "column3", Short.class,
+        "column4", Double.class,
+        "column5", String.class,
+        "column6", Integer.class,
+        "column7", Boolean.class);
+  }
+}
+
+// IntegerStringBooleanFloatDoubleLongShort
