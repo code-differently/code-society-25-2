a4a2b6367da771680c58b87a85df923f37fc0711
Jared Edge <jarededgewrk@gmail.com>
2025-08-27 21:14:10 +0000

    feat: adds correct files into new branch for lesson 6 HW PR

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,7 +2,25 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+    const sum = this.add(c, d);
+
+    const denominator = this.pow(sum, e);
+
+    const numerator = this.multiply(a, b);
+
+    return this.divide(numerator, denominator);
+  }
+  add(a: number, b: number): number {
+    return a + b;
+  }
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+  divide(a: number, b: number): number {
+    if (b === 0) {
+      throw new Error("Can't divide by zero");
+    }
+    return a / b;
   }
 
   pow(base: number, exponent: number): number {
