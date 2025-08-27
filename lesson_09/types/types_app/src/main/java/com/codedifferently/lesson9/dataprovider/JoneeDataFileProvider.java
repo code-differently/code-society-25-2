1740bb53491fd5b38579e16fd4f4f6d34228d2c3
jmckellar252-png <jmckellar252@gmail.com>
2025-08-27 02:33:53 +0000

    feat: add JoneeDataFileProvider component

--- lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/JoneeDataFileProvider.java
+++ lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/JoneeDataFileProvider.java
@@ -1,26 +1,27 @@
 package com.codedifferently.lesson9.dataprovider;
 
 import com.codedifferently.lesson9.model.DataType;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import org.springframework.stereotype.Component;
+import java.util.Collections;
+import java.util.Map;
+import java.util.LinkedHashMap;
 
 @Component
 public class JoneeDataFileProvider implements DataProvider {
 
-  @Override
-  public String getProviderName() {
-    return "joneeData";
-  }
+    @Override
+    public String getProviderName() {
+        return "joneeData";
+    }
 
-  @Override
-  public Map<String, DataType> getTypes() {
-    Map<String, DataType> types = new LinkedHashMap<>();
-    types.put("itemCode", DataType.STRING);
-    types.put("quantity", DataType.INTEGER);
-    types.put("price", DataType.DOUBLE);
-    types.put("isInStock", DataType.BOOLEAN);
-    return Collections.unmodifiableMap(types);
-  }
+    @Override
+    public Map<String, DataType> getTypes() {
+        Map<String, DataType> columnTypes = new LinkedHashMap<>();
+        columnTypes.put("id", DataType.INTEGER);
+        columnTypes.put("firstName", DataType.STRING);
+        columnTypes.put("lastName", DataType.STRING);
+        columnTypes.put("age", DataType.INTEGER);
+        columnTypes.put("isActive", DataType.BOOLEAN);
+        return Collections.unmodifiableMap(columnTypes);
+    }
 }
