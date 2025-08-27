af76578dcb50834a83736ca669810fb18d030808
Jared Edge <jarededgewrk@gmail.com>
2025-08-27 20:45:29 +0000

    feat: adds  place .env.test correctly; HW_VERSION=D; calculator uses helpers

--- lesson_06/jarededge/expression_calculator.ts
+++ lesson_06/jarededge/expression_calculator.ts
@@ -2,13 +2,25 @@ export class ExpressionCalculator {
   /** Returns a calculation involving a, b, c, d, and e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    const sum = c + d;
+    const sum = this.add(c, d);
 
     const denominator = this.pow(sum, e);
 
-    const numerator = a * b;
+    const numerator = this.multiply(a, b);
 
-    return numerator / denominator;
+    return this.divide(numerator, denominator);
+  }
+  add(a:number, b:number): number {
+    return a + b;
+  }
+  multiply(a:number, b:number): number {
+    return a * b;
+  }
+  divide(a:number, b:number): number {
+    if(b === 0) {
+      throw new Error("Can't divide by zero");
+    } 
+    return a / b;
   }
 
   pow(base: number, exponent: number): number {
