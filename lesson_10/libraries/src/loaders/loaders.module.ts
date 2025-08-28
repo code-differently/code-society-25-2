938af494b2d47cecdaf80e956a1f44d99fb1ba18
jaydenellis <jlellis252@gmail.com>
2025-08-28 19:49:43 +0000

    finsihed lesson_10 work

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
-
+import { JaydenEllisLoader } from './jayden_ellis_loader.js';
+   
 export const Loaders = Symbol.for('Loaders');
 
 const LOADER_PROVIDERS = [
   AnthonyMaysLoader,
+  JaydenEllisLoader
 ];
 
 @Module({
