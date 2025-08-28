7cce0fe7107261ca2cbf581e1d231232db7f5b1e
calvin <cwrobinson252@mail.com>
2025-08-28 21:04:18 +0000

    feat: add CalvinRobinsonLoader with CSV loading

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
+import { CalvinRobinsonLoader } from './calvin_robinson_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
 const LOADER_PROVIDERS = [
   AnthonyMaysLoader,
+  CalvinRobinsonLoader,
 ];
 
 @Module({
