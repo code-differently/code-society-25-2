0cf524ac9b0a180f55cbe43f19f0f0abb07c224e
Daniel Boyce <danielboyce20@gmail.com>
2025-08-28 19:31:33 +0000

    changes jsonfile generator to be its own class instead of subclass of sample file generator

--- lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/generator/JsonFileGenerator.java
+++ lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/generator/JsonFileGenerator.java
@@ -13,7 +13,13 @@ import com.codedifferently.lesson9.generator.Generators.IntValueGenerator;
 import com.codedifferently.lesson9.generator.Generators.LongValueGenerator;
 import com.codedifferently.lesson9.generator.Generators.ShortValueGenerator;
 import com.codedifferently.lesson9.generator.Generators.StringValueGenerator;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,14 +29,13 @@ import org.springframework.stereotype.Service;
  * @author vscode
  */
 @Service
-public class JsonFileGenerator extends SampleFileGenerator {
+public class JsonFileGenerator {
 
   // Refrences to all DataProvider beans
   @Autowired private List<DataProvider> dataProviders;
 
   // overriding the createTestFile method to generate files for each DataProvider
-  @Override
-  public void createTestFile(String path, String providerName) {
+  public void createTestFile(String path) {
 
     for (DataProvider provider : dataProviders) {
 
@@ -87,5 +92,35 @@ public class JsonFileGenerator extends SampleFileGenerator {
     }
 
     return generators;
+  }
+
+  protected ArrayList<Map<String, String>> createSampleData(List<ValueGenerator> generators) {
+    var rows = new ArrayList<Map<String, String>>();
+    for (var i = 0; i < 10; ++i) {
+      Map<String, String> row = createRow(generators);
+      rows.add(createRow(generators));
+    }
+    return rows;
+  }
+
+  protected Map<String, String> createRow(List<ValueGenerator> generators) {
+    var row = new LinkedHashMap<String, String>();
+    for (int i = 0; i < generators.size(); ++i) {
+      var columnIndex = i + 1;
+      row.put("column" + columnIndex, generators.get(i).generateValue());
+    }
+    return row;
+  }
+
+  protected void saveToJsonFile(
+      String path, String providerName, ArrayList<Map<String, String>> rows) {
+    var file = new File(path + File.separator + providerName + ".json");
+    file.getParentFile().mkdirs();
+    var gson = new GsonBuilder().setPrettyPrinting().create();
+    try (var writer = new FileWriter(file, false)) {
+      writer.write(gson.toJson(rows));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
