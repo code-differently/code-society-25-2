77da1264449ecc0ceb5d8e24bbc6066879ba8e77
tobyev <teevans252@gmail.com>
2025-08-29 16:40:30 +0000

    feat(lesson-10): add TobyEvansLoader and update loaders.module.ts

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
-import { BenjaminScottLoader } from './benjamin_scott_loader.js';
+import { TobyEvansLoader } from './toby_evans_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
-const LOADER_PROVIDERS = [AnthonyMaysLoader, BenjaminScottLoader];
+const LOADER_PROVIDERS = [AnthonyMaysLoader, TobyEvansLoader];
 
 @Module({
   providers: [
     ...LOADER_PROVIDERS,
     {
       provide: Loaders,
-      useFactory: (...args) => [...args],
+      useFactory: (...args: any[]) => [...args],
       inject: LOADER_PROVIDERS,
     },
   ],
