f47f3437435a5a6488ae8e919fc8e0151a3d6c8c
Joy Brown <jxbrown252@gmail.com>
2025-08-30 17:15:45 -0400

    feat(lesson11): implement getConcatenation & findWordsContaining; run spotless

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -1,22 +1,35 @@
 package com.codedifferently.lesson11;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lesson11 {
 
   /**
-   * Provide the solution to LeetCode 1929 here:
-   * https://leetcode.com/problems/concatenation-of-array
+   * LeetCode 1929: Concatenation of Array Make a new array that has the input twice, back-to-back.
+   * Example: [1,2,1] -> [1,2,1, 1,2,1]
    */
   public int[] getConcatenation(int[] nums) {
-    return null;
+    int n = nums.length;
+    int[] ans = new int[2 * n];
+    for (int i = 0; i < n; i++) {
+      ans[i] = nums[i]; // first copy
+      ans[i + n] = nums[i]; // second copy
+    }
+    return ans;
   }
 
   /**
-   * Provide the solution to LeetCode 2942 here:
-   * https://leetcode.com/problems/find-words-containing-character/
+   * LeetCode 2942: Find Words Containing Character Return the list of indices where words[i]
+   * contains the character x. Example: ["leet","code"], x='e' -> [0,1]
    */
   public List<Integer> findWordsContaining(String[] words, char x) {
-    return null;
+    List<Integer> result = new ArrayList<>();
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].indexOf(x) != -1) {
+        result.add(i);
+      }
+    }
+    return result;
   }
 }
