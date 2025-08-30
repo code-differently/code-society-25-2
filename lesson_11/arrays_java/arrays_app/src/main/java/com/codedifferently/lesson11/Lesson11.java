e273b06c49dffb137da9f6a23cf46d1c5f5dbd30
lquinoa252 <lquinoa252@gmail.com>
2025-08-30 17:19:24 +0000

    feat: Implements array problems

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -9,7 +9,12 @@ public class Lesson11 {
    * https://leetcode.com/problems/concatenation-of-array
    */
   public int[] getConcatenation(int[] nums) {
-    return null;
+    int[] result = new int[nums.length * 2];
+    for (int i = 0; i < nums.length; i++) {
+      result[i] = nums[i];
+      result[i + nums.length] = nums[i];
+    }
+    return result;
   }
 
   /**
@@ -17,6 +22,13 @@ public class Lesson11 {
    * https://leetcode.com/problems/find-words-containing-character/
    */
   public List<Integer> findWordsContaining(String[] words, char x) {
-    return null;
+
+    List<Integer> result = new ArrayList<>();
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].indexOf(x) != -1) {
+        result.add(i);
+      }
+    }
+    return result;
   }
 }
