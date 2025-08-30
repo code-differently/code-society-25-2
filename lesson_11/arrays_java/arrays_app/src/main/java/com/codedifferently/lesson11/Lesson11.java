bf2dc009ec1e86e5330ed5ebe02e1b7f3ab273e8
Tyran Rice <tyranricejr@gmail.com>
2025-08-30 19:00:01 +0000

    feat: adds Tyran's Lesson 11 java and ts leetcode functions

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -1,5 +1,6 @@
 package com.codedifferently.lesson11;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lesson11 {
@@ -9,7 +10,15 @@ public class Lesson11 {
    * https://leetcode.com/problems/concatenation-of-array
    */
   public int[] getConcatenation(int[] nums) {
-    return null;
+    int n = nums.length;
+    int[] result = new int[2 * n];
+
+    for (int i = 0; i < n; i++) {
+      result[i] = nums[i];
+      result[i + n] = nums[i];
+    }
+
+    return result;
   }
 
   /**
@@ -17,6 +26,12 @@ public class Lesson11 {
    * https://leetcode.com/problems/find-words-containing-character/
    */
   public List<Integer> findWordsContaining(String[] words, char x) {
-    return null;
+    ArrayList<Integer> result = new ArrayList();
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].indexOf(x) != -1) {
+        result.add(i);
+      }
+    }
+    return result;
   }
 }
