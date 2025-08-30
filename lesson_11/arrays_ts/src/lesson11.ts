04d10cf0753bc38a3b38a3197fb1559f37e93446
brooklynharden <hardenbrooklyn123@gmail.com>
2025-08-30 15:19:43 +0000

    feat: completed extra credit lesson 11

--- lesson_11/arrays_ts/src/lesson11.ts
+++ lesson_11/arrays_ts/src/lesson11.ts
@@ -3,7 +3,15 @@
  * https://leetcode.com/problems/concatenation-of-array
  */
 export function getConcatenation(nums: number[]): number[] {
-  return [];
+  if (nums.length === 0) {
+    return nums;
+  }
+  const ans: number[] = [nums.length * 2];
+  for (let i = 0; i < nums.length; i++) {
+    ans[i] = nums[i];
+    ans[i + nums.length] = nums[i];
+  }
+  return ans;
 }
 
 /**
@@ -11,5 +19,15 @@ export function getConcatenation(nums: number[]): number[] {
  * https://leetcode.com/problems/find-words-containing-character/
  */
 export function findWordsContaining(words: string[], x: string): number[] {
-  return [];
+  if (words.length === 0) {
+    return [];
+  }
+
+  const result: number[] = [];
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].indexOf(x) != -1) {
+      result.push(i);
+    }
+  }
+  return result;
 }
