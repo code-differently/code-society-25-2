9e8ddb8a7dd7177a9690b25a78b3ec2b34f6b143
jmckellar252-png <jmckellar252@gmail.com>
2025-08-31 00:35:16 +0000

    style: apply code formatting with spotlessApply

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -7,8 +7,8 @@ public class Lesson11 {
   public int[] getConcatenation(int[] nums) {
     int[] result = new int[nums.length * 2];
     for (int i = 0; i < nums.length; i++) {
-        result[i] = nums[i];
-        result[i + nums.length] = nums[i];
+      result[i] = nums[i];
+      result[i + nums.length] = nums[i];
     }
     return result;
   }
@@ -20,9 +20,9 @@ public class Lesson11 {
   public List<Integer> findWordsContaining(String[] words, char x) {
     List<Integer> result = new ArrayList<>();
     for (int i = 0; i < words.length; i++) {
-        if (words[i].indexOf(x) != -1) {
-            result.add(i);
-        }
+      if (words[i].indexOf(x) != -1) {
+        result.add(i);
+      }
     }
     return result;
   }
