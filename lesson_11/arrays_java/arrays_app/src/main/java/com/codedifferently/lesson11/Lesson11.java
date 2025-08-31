405caf28db89bb85cfe58d1f50ada0d9572fd193
jmckellar252-png <jmckellar252@gmail.com>
2025-08-31 00:31:24 +0000

    feat: implement array concatenation and word search methods
    - Add getConcatenation method for LeetCode 1929
    - Add findWordsContaining method for LeetCode 2942
    - Complete Lesson11.java assignment requirements

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -1,7 +1,3 @@
-package com.codedifferently.lesson11;
-
-import java.util.List;
-
 public class Lesson11 {
 
   /**
@@ -9,7 +5,12 @@ public class Lesson11 {
    * https://leetcode.com/problems/concatenation-of-array
    */
   public int[] getConcatenation(int[] nums) {
-    return null;
+    int[] result = new int[nums.length * 2];
+    for (int i = 0; i < nums.length; i++) {
+        result[i] = nums[i];
+        result[i + nums.length] = nums[i];
+    }
+    return result;
   }
 
   /**
@@ -17,6 +18,12 @@ public class Lesson11 {
    * https://leetcode.com/problems/find-words-containing-character/
    */
   public List<Integer> findWordsContaining(String[] words, char x) {
-    return null;
+    List<Integer> result = new ArrayList<>();
+    for (int i = 0; i < words.length; i++) {
+        if (words[i].indexOf(x) != -1) {
+            result.add(i);
+        }
+    }
+    return result;
   }
 }
