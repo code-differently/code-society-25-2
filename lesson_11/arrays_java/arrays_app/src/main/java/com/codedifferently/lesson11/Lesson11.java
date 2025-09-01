20ecb14564165dec1621dcd39ea56dac23afd9a1
Trinitie Jackson <txjackson252@gmail.com>
2025-09-01 05:00:31 +0000

    chore: saving java file

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -5,39 +5,33 @@ import java.util.List;
 
 public class Lesson11 {
 
-  /**
-   * Provide the solution to LeetCode 1929 here:
-   * https://leetcode.com/problems/concatenation-of-array
-   */
+
   public int[] getConcatenation(int[] nums) {
     int n = nums.length;
-    int[] ans = new int[2 * n];
-
+        int[] ans = new int[2 * n];
+    
     for (int i = 0; i < n; i++) {
       ans[i] = nums[i];
       ans[i + n] = nums[i];
+}
+    return ans;  
     }
-    return ans;
-  }
 
-  /**
-   * Provide the solution to LeetCode 2942 here:
-   * https://leetcode.com/problems/find-words-containing-character/
-   */
+
   public List<Integer> findWordsContaining(String[] words, char x) {
     ArrayList<Integer> indices = new ArrayList<>();
 
-    for (int i = 0; i < words.length; i++) {
-      String word = words[i];
-      if (word.indexOf(x) != -1) {
-        indices.add(i);
-      }
+    for (int i = 0; i < words.length; i++){
+        String word = words[i];
+        if (word.indexOf(x) != -1) {
+            indices.add(i);
+     }
     }
-
+    
     int[] result = new int[indices.size()];
     for (int i = 0; i < indices.size(); i++) {
-      result[i] = indices.get(i);
+             result[i] = indices.get(i);
     }
     return indices;
-  }
+     }
 }
