3bae23d97124ed81a12ae875149d3e7f5ba3fb7b
Talia Crockett <tmcrockett252@gmail.com>
2025-09-02 15:01:43 +0000

    feat: add Stack implementation and gameResult method

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,25 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+    int evenTeam = 0;     
+    int oddTeam = 0;  
+
+    ListNode current = head; // Start from the head of the list
+    while (current != null && current.next != null) {
+      if (current.val > current.next.val) {
+        evenTeam++;
+      } else {
+        oddTeam++;
+      }
+      current = current.next.next; // Move to the next pair
+    }
+
+    if (evenTeam == oddTeam) {
+      return "Tie";
+    } else if (evenTeam > oddTeam) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
   }
 }
