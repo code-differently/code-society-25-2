301c2e246953f3ba017d56bf01ed38c1ad4a91ba
emblue252 <emblue252@gmail.com>
2025-09-02 01:26:34 +0000

    feat: adds files for lesson_11 assignmnet and extra credit

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -9,7 +9,13 @@ public class Lesson11 {
    * https://leetcode.com/problems/concatenation-of-array
    */
   public int[] getConcatenation(int[] nums) {
-    return null;
+        int n = nums.length; 
+        int[] ans = new int[ n + n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+            return ans;
   }
 
   /**
@@ -17,6 +23,16 @@ public class Lesson11 {
    * https://leetcode.com/problems/find-words-containing-character/
    */
   public List<Integer> findWordsContaining(String[] words, char x) {
-    return null;
+     List<Integer> ans = new ArrayList<>(); 
+
+        for (int i = 0; i < words.length; i++) {  
+            for (int j = 0; j < words[i].length(); j++) {  
+                if (words[i].charAt(j) == x) {
+                    ans.add(i);
+                    break;          
+                }
+            }
+        }
+        return ans;  
   }
 }
--- lesson_11/arrays_ts/src/lesson11.ts
+++ lesson_11/arrays_ts/src/lesson11.ts
@@ -3,7 +3,15 @@
  * https://leetcode.com/problems/concatenation-of-array
  */
 export function getConcatenation(nums: number[]): number[] {
-  return [];
+  let n = nums.length;
+    let ans: number[] = [];
+    
+    for (let i = 0; i < n; i++) {
+        ans[i] = nums[i];
+        ans[i + n] = nums[i];
+
+    }
+        return ans;
 }
 
 /**
@@ -11,5 +19,16 @@ export function getConcatenation(nums: number[]): number[] {
  * https://leetcode.com/problems/find-words-containing-character/
  */
 export function findWordsContaining(words: string[], x: string): number[] {
-  return [];
+    const result: number[] = [];
+
+    for (let i = 0; i < words.length; i++) {
+        for (let j = 0; j < words[i].length; j++) {
+            if (words[i].charAt(j) === x) {
+                result.push(i)
+                break;
+            }
+        }
+    }
+
+    return result;
 }
