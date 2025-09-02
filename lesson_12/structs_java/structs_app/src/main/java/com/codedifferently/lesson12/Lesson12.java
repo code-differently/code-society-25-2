7f6aa6717bfff0ed53766b02881d76380c2b3454
Kerry Ferguson <kferg2021@gmail.com>
2025-09-02 14:50:08 +0000

    feat: adds completed lesson_12 methods in Java and TypeScript

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,33 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+    int evenPoints = 0;
+    int oddPoints = 0;
+
+    // Traverse the list in pairs
+    ListNode current = head;
+    while (current != null && current.next != null) {
+      // current is at even index, current.next is at odd index
+      int evenValue = current.val;
+      int oddValue = current.next.val;
+
+      if (evenValue > oddValue) {
+        evenPoints++;
+      } else if (oddValue > evenValue) {
+        oddPoints++;
+      }
+      // If equal, no points awarded (though this shouldn't happen based on constraints)
+
+      // Move to the next pair
+      current = current.next.next;
+    }
+
+    if (evenPoints > oddPoints) {
+      return "Even";
+    } else if (oddPoints > evenPoints) {
+      return "Odd";
+    } else {
+      return "Tie";
+    }
   }
 }
--- lesson_12/structs_ts/src/lesson12.ts
+++ lesson_12/structs_ts/src/lesson12.ts
@@ -6,6 +6,33 @@ export class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public gameResult(head: ListNode | null): string {
-    return '';
+    let evenPoints = 0;
+    let oddPoints = 0;
+
+    // Traverse the list in pairs
+    let current = head;
+    while (current !== null && current.next !== undefined) {
+      // current is at even index, current.next is at odd index
+      const evenValue = current.val;
+      const oddValue = current.next.val;
+
+      if (evenValue > oddValue) {
+        evenPoints++;
+      } else if (oddValue > evenValue) {
+        oddPoints++;
+      }
+      // If equal, no points awarded (though this shouldn't happen based on constraints)
+
+      // Move to the next pair
+      current = current.next.next || null;
+    }
+
+    if (evenPoints > oddPoints) {
+      return 'Even';
+    } else if (oddPoints > evenPoints) {
+      return 'Odd';
+    } else {
+      return 'Tie';
+    }
   }
 }
