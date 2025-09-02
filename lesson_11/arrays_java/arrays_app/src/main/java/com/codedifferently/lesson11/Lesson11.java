003d5840a9cb7e5b58bd18c32a573e4933e4b81b
jaydenellis <jlellis252@gmail.com>
2025-09-02 20:47:41 +0000

    lesson_11 work complete

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -1,22 +1,31 @@
 package com.codedifferently.lesson11;
 
+import java.util.*;
 import java.util.List;
 
 public class Lesson11 {
 
-  /**
-   * Provide the solution to LeetCode 1929 here:
-   * https://leetcode.com/problems/concatenation-of-array
-   */
   public int[] getConcatenation(int[] nums) {
-    return null;
+    int n = nums.length;
+    int[] ans = new int[2 * n];
+
+    for (int i = 0; i < n; i++) {
+      ans[i] = nums[i]; // put nums[i] in the first half
+      ans[i + n] = nums[i]; // put nums[i] in the second half
+    }
+
+    return ans;
   }
 
-  /**
-   * Provide the solution to LeetCode 2942 here:
-   * https://leetcode.com/problems/find-words-containing-character/
-   */
   public List<Integer> findWordsContaining(String[] words, char x) {
-    return null;
+    List<Integer> ans = new ArrayList<>();
+
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].indexOf(x) != -1) { // if word contains x
+        ans.add(i); // store the index
+      }
+    }
+
+    return ans;
   }
 }
--- lesson_11/arrays_ts/src/lesson11.ts
+++ lesson_11/arrays_ts/src/lesson11.ts
@@ -1,15 +1,26 @@
-/**
- * Provide the solution to LeetCode 1929 here:
- * https://leetcode.com/problems/concatenation-of-array
- */
-export function getConcatenation(nums: number[]): number[] {
-  return [];
-}
+export class Lesson11 {
 
-/**
- * Provide the solution to LeetCode 2942 here:
- * https://leetcode.com/problems/find-words-containing-character/
- */
-export function findWordsContaining(words: string[], x: string): number[] {
-  return [];
+  getConcatenation(nums: number[]): number[] {
+    const n = nums.length;
+    const ans: number[] = new Array(2 * n);
+
+    for (let i = 0; i < n; i++) {
+      ans[i] = nums[i];       // first half
+      ans[i + n] = nums[i];   // second half
+    }
+
+    return ans;
+  }
+
+  findWordsContaining(words: string[], x: string): number[] {
+    const ans: number[] = [];
+
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].indexOf(x) !== -1) { // if word contains x
+        ans.push(i); // store the index
+      }
+    }
+
+    return ans;
+  }
 }
