bc288da869e7efe267542c1c8b33a17886727fe5
jmckellar252-png <jmckellar252@gmail.com>
2025-09-03 14:36:14 +0000

    feat: implement gameResult method and Stack isEmpty/peek methods

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,27 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+    int oddScore = 0;
+    int evenScore = 0;
+
+    while (head != null && head.next != null) {
+      int firstValue = head.val;
+      int secondValue = head.next.val;
+
+      if (firstValue > secondValue) {
+        evenScore++; // Even team gets a point
+      } else if (secondValue > firstValue) {
+        oddScore++; // Odd team gets a point
+      }
+      head = head.next.next; // Move to next pair
+    }
+
+    if (oddScore > evenScore) {
+      return "Odd";
+    } else if (evenScore > oddScore) {
+      return "Even";
+    } else {
+      return "Tie";
+    }
   }
 }
