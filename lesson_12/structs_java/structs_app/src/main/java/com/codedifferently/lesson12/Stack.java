52e1c93925079a1d0cea48b331821861a0cad5cd
bscott519 <bbscott252@gmail.com>
2025-09-03 15:21:47 +0000

    feat: completed Stack methods

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Stack.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Stack.java
@@ -10,17 +10,34 @@ public class Stack {
 
   public void push(int value) {
     // Your code here
+    ListNode newNode = new ListNode(value);
+    newNode.next = top;
+    top = newNode;
   }
 
   public int pop() {
-    return 0;
+    if (isEmpty()) {
+      throw new RuntimeException("Stack is empty");
+    } else {
+      int value = top.val;
+      top = top.next;
+      return value;
+    }
   }
 
   public int peek() {
-    return 0;
+    if (isEmpty()) {
+      throw new RuntimeException("Stack is empty");
+    } else {
+      return top.val;
+    }
   }
 
   public boolean isEmpty() {
-    return true;
+    if (top == null) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
