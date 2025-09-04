2a5200fa92206faa075714bac22cd4ab4bf761b9
Tyran Rice <tyranricejr@gmail.com>
2025-09-04 13:55:25 +0000

    feat: adds Tyran's Lesson 13 Leetcode 3146 in Java & TS

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -3,5 +3,19 @@
  * https://leetcode.com/problems/permutation-difference-between-two-strings
  */
 export function findPermutationDifference(s: string, t: string): number {
-  return 0;
+  const perm1: Record<string, number> = {};
+  const perm2: Record<string, number> = {};
+
+  // build maps
+  for (let i = 0; i < s.length; i++) {
+    perm1[s[i]] = i;
+    perm2[t[i]] = i;
+  }
+
+  let count = 0;
+  for (const key in perm1) {
+    count += Math.abs(perm1[key] - perm2[key]);
+  }
+
+  return count;
 }
