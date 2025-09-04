ebc2c5e645ec953b9b1e2fb8e23bf089d1b0a681
jaydenellis <jlellis252@gmail.com>
2025-09-04 18:45:39 +0000

    feat/lesson_13 completed

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,5 +1,8 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Lesson13 {
 
   /**
@@ -7,6 +10,22 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+
+    Map<Character, Integer> tMap = new HashMap<>();
+    for (int i = 0; i < t.length(); i++) {
+      tMap.put(t.charAt(i), i);
+    }
+    int total = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+      char current = s.charAt(i);
+
+      int indexInT = tMap.get(current);
+
+      int diff = Math.abs(i - indexInT);
+      total += diff;
+    }
+
+    return total;
   }
 }
--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -1,7 +1,22 @@
-/**
- * Provide the solution to LeetCode 3146 here:
- * https://leetcode.com/problems/permutation-difference-between-two-strings
- */
-export function findPermutationDifference(s: string, t: string): number {
-  return 0;
+function findPermutationDifference(s: string, t: string): number {
+  // Map characters of t to their index
+  const tMap: Record<string, number> = {};
+  for (let i = 0; i < t.length; i++) {
+    tMap[t[i]] = i;
+  }
+
+  let total = 0;
+
+  // Compare indexes from s and t
+  for (let i = 0; i < s.length; i++) {
+    const current = s[i];
+    const indexInT = tMap[current];
+    const diff = Math.abs(i - indexInT);
+    total += diff;
+  }
+
+  return total;
 }
+
+
+
