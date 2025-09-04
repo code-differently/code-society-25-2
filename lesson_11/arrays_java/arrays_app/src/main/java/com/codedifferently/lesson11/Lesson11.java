1cea714dca27f4e2fbe9ee744be98fc87b70b461
dxbenson252 <dxbenson252@gmail.com>
2025-09-04 14:49:27 +0000

    Added public static void

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -3,26 +3,24 @@ package com.codedifferently.lesson11;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Lesson11 {
-
+public class Lesson11  {
+public static void main(String[] args) {
+  
+}
   /**
    * Provide the solution to LeetCode 1929 here:
    * https://leetcode.com/problems/concatenation-of-array
    */
- 
-    public int[] getConcatenation(int[] nums) {
-        int n = nums.length;
-        int[] my_new_arr = new int[n * 2];   
+  public int[] getConcatenation(int[] nums) {
+    int n = nums.length;
+    int[] my_new_arr = new int[n * 2];
 
-  
-        for (int i = 0; i < n; i++) {        
-            my_new_arr[i] = nums[i];         
-            my_new_arr[i + n] = nums[i];     // second copy offset by n
-        }
-        return my_new_arr;                  
+    for (int i = 0; i < n; i++) {
+      my_new_arr[i] = nums[i];
+      my_new_arr[i + n] = nums[i]; // second copy offset by n
     }
-
-
+    return my_new_arr;
+  }
 
   /**
    * Provide the solution to LeetCode 2942 here:
@@ -31,12 +29,11 @@ public class Lesson11 {
   public List<Integer> findWordsContaining(String[] words, char x) {
     List<Integer> my_array = new ArrayList<>();
 
-        for (int i = 0; i < words.length; i++) {
-        
-            if (words[i].indexOf(x) >= 0) {
-                my_array.add(i);
-        }
-    
+    for (int i = 0; i < words.length; i++) {
+
+      if (words[i].indexOf(x) >= 0) {
+        my_array.add(i);
+      }
     }
     return my_array;
   }
