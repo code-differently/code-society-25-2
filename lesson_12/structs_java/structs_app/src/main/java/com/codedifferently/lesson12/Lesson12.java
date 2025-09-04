40a2ddfd675c13374c6ee9600590e223c34425af
Joy Brown <jxbrown252@gmail.com>
2025-09-04 19:07:42 +0000

    feat(lesson12): implement gameResult and ListNode-backed Stack (Java)

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -3,10 +3,29 @@ package com.codedifferently.lesson12;
 public class Lesson12 {
 
   /**
-   * Provide the solution to LeetCode 3062 here:
-   * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
+   * Compare nodes in pairs: (a,b), (c,d), ...
+   * a>b => Even gets a point; a<b => Odd gets a point; equal => no points.
+   * Return "Even", "Odd", or "Tie".
    */
   public String gameResult(ListNode head) {
-    return null;
+    int evenPoints = 0;
+    int oddPoints = 0;
+
+    ListNode curr = head;
+    while (curr != null && curr.next != null) {
+      int first = curr.val;       // if your ListNode uses 'value', change to curr.value
+      int second = curr.next.val; // same note as above
+
+      if (first > second) {
+        evenPoints++;
+      } else if (first < second) {
+        oddPoints++;
+      }
+      curr = curr.next.next; // move to next pair
+    }
+
+    if (evenPoints > oddPoints) return "Even";
+    if (oddPoints > evenPoints) return "Odd";
+    return "Tie";
   }
 }
--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Stack.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Stack.java
@@ -1,6 +1,6 @@
 package com.codedifferently.lesson12;
 
-/** Implement the below Stack by providing code for the class methods. */
+/** Linked-list-backed stack. */
 public class Stack {
   private ListNode top;
 
@@ -8,19 +8,33 @@ public class Stack {
     this.top = null;
   }
 
+  /** Push value onto the stack. */
   public void push(int value) {
-    // Your code here
+    ListNode node = new ListNode(value); // assumes constructor taking (int)
+    node.next = top;
+    top = node;
   }
 
+  /** Pop and return top value. Throws if empty. */
   public int pop() {
-    return 0;
+    if (top == null) {
+      throw new IllegalStateException("Stack is empty");
+    }
+    int val = top.val; // change to top.value if your node field is named 'value'
+    top = top.next;
+    return val;
   }
 
+  /** Return top value without removing. Throws if empty. */
   public int peek() {
-    return 0;
+    if (top == null) {
+      throw new IllegalStateException("Stack is empty");
+    }
+    return top.val; // or top.value
   }
 
+  /** Is the stack empty? */
   public boolean isEmpty() {
-    return true;
+    return top == null;
   }
 }
