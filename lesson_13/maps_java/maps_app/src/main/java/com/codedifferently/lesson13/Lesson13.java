3a2e3634922e51e2c88eca02c109304976b6f4d1
jmckellar252-png <jmckellar252@gmail.com>
2025-09-04 17:49:20 +0000

    style: apply Spotless formatting

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -12,17 +12,17 @@ public class Lesson13 {
     HashMap<Character, Integer> positionMap = new HashMap<>();
 
     for (int i = 0; i < s.length(); i++) {
-        positionMap.put(s.charAt(i), i);
+      positionMap.put(s.charAt(i), i);
     }
 
     int totalDifference = 0;
     for (int i = 0; i < t.length(); i++) {
-        char currentChar = t.charAt(i);
-        int positionInS = positionMap.get(currentChar);
-        int positionInT = i;
-        
-        int difference = Math.abs(positionInS - positionInT);
-        totalDifference += difference;
+      char currentChar = t.charAt(i);
+      int positionInS = positionMap.get(currentChar);
+      int positionInT = i;
+
+      int difference = Math.abs(positionInS - positionInT);
+      totalDifference += difference;
     }
 
     return totalDifference;
