b04ee8edeecb704d9f52bcfb622b8edff18fc414
Danielson A. <adjocydanielson@gmail.com>
2025-09-04 15:11:22 +0000

    feat:lesson 13 Java done and tested

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,12 +1,27 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+
 public class Lesson13 {
+
+  public static void main(String[] args) {
+    
+  }
 
   /**
    * Provide the solution to LeetCode 3146 here:
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    int sum = 0;
+    HashMap<Character, Integer> indexofCharS = new HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+      indexofCharS.put(s.charAt(i), i);
+    }
+    for (int i = 0; i < t.length(); i++) {
+      char ch = t.charAt(i);
+      sum += Math.abs(indexofCharS.get(ch) - i);
+    }
+    return sum;
   }
 }
