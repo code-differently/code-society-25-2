43a51377ecee5a02565cafe4c4473de491e93f5a
Danielson A. <adjocydanielson@gmail.com>
2025-09-04 15:19:34 +0000

    fix: map types specified

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -4,7 +4,7 @@
  */
 export function findPermutationDifference(s: string, t: string): number {
     let sum = 0;
-    const indexofCharS: Map<string, number> = new Map();
+    const indexofCharS = new Map<string, number>();
     for (let i = 0; i < s.length; i++) {
         indexofCharS.set(s.charAt(i), i);
     }
