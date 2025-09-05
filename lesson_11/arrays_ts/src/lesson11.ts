c49015556f6dd4bd6886b23b7130441fa47561a9
Trinitie Jackson <txjackson252@gmail.com>
2025-09-05 01:38:59 +0000

    chore: fix formatting

--- lesson_11/arrays_ts/src/lesson11.ts
+++ lesson_11/arrays_ts/src/lesson11.ts
@@ -1,16 +1,17 @@
 
 export function getConcatenation(nums: number[]): number[] {
-  const n = nums.length;
-   const ans: number[] = new Array(2 * n);
-for (let i = 0; i < n; i++) {
-    ans[i] = nums[i];
-    ans[i + n] = nums[i];
-}
+    const n = nums.length;
+    const ans: number[] = new Array(2 * n);
+
+    for (let i = 0; i < n; i++) {
+        ans[i] = nums[i];
+        ans[i + n] = nums[i];
+    }
         return ans;  
 }
 
 export function findWordsContaining(words: string[], x: string): number[] {
-const indices: number[] = [];
+    const indices: number[] = [];
 
     for (let i = 0; i < words.length; i++){
         const word = words[i];
