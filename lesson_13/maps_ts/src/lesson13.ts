c31812cf014a76cc962f9c356ea43229895bfa27
Martha <iofuani252@gmail.com>
2025-09-05 16:43:07 +0000

    feat: adds Martha's completed lesson 13 TS file

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -3,5 +3,13 @@
  * https://leetcode.com/problems/permutation-difference-between-two-strings
  */
 export function findPermutationDifference(s: string, t: string): number {
-  return 0;
+  const permutationArray: number[] = [128]; 
+        for (let i = 0; i < s.length; i++) {
+            permutationArray[s.charCodeAt(i)] = i;
+        }
+        let sum = 0;
+        for (let i = 0; i < t.length; i++) {
+            sum += Math.abs(permutationArray[t.charCodeAt(i)] - i);  
+        }
+            return sum;
 }
