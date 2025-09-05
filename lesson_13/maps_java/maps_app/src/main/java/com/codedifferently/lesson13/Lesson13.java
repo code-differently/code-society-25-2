d1680cb875aab203a804e515c8d0215054ab54d0
Kerry Ferguson <kferg2021@gmail.com>
2025-09-05 13:29:40 +0000

    feat: adds completed lesson_13 assignment in Java and TypeScript

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,5 +1,8 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Lesson13 {
 
   /**
@@ -7,6 +10,24 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    // Create a map to store the index of each character in string s
+    Map<Character, Integer> indexbyChar = new HashMap<>();
+
+    // Store the index of each character in string s
+    for (int i = 0; i < s.length(); i++) {
+      indexbyChar.put(s.charAt(i), i);
+    }
+
+    int permutationDifference = 0;
+
+    // For each character in string t, find its index in string s
+    // and calculate the absolute difference
+    for (int i = 0; i < t.length(); i++) {
+      char currentChar = t.charAt(i);
+      int currentIndex = indexbyChar.get(currentChar);
+      permutationDifference += Math.abs(i - currentIndex);
+    }
+
+    return permutationDifference;
   }
 }
