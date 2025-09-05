336f8dfeb2dde63168966d021eacfe845061581d
Trinitie Jackson <txjackson252@gmail.com>
2025-09-05 02:07:39 +0000

    chore: java re-formatting

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -5,11 +5,13 @@ import java.util.Map;
 
 public class Lesson13 {
   public int findPermutationDifference(String s, String t) {
+
     Map<Character, Integer> map = new HashMap<>();
 
     for (int i = 0; i < t.length(); i++) {
       map.put(t.charAt(i), i);
     }
+
     int sum = 0;
 
     for (int i = 0; i < s.length(); i++) {
