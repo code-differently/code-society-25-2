d953913e6cc8834696bc5064c029cc731932d3f4
dxbenson252 <dxbenson252@gmail.com>
2025-09-05 16:06:05 +0000

    Finished Lesson 13 & Credit

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -3,5 +3,20 @@
  * https://leetcode.com/problems/permutation-difference-between-two-strings
  */
 export function findPermutationDifference(s: string, t: string): number {
-  return 0;
+  // Map each character to its index in s and t
+  const sIndex = new Map<string, number>();
+  const tIndex = new Map<string, number>();
+  
+  for (let i = 0; i < s.length; i++) {
+    sIndex.set(s[i], i);
+    tIndex.set(t[i], i);
+  }
+  
+  let diff = 0;
+  for (let i = 0; i < s.length; i++) {
+    const c = s[i];
+    diff += Math.abs(sIndex.get(c)! - tIndex.get(c)!);
+  }
+  
+  return diff;
 }
