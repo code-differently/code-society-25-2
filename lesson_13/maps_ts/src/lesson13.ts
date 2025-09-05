351ad5d3bb0c40be088b3a56b9c47143d8a8044b
tobyev <teevans252@gmail.com>
2025-09-05 16:00:02 +0000

    feat(lesson-13-ts): implement findPermutationDifference in lesson13.ts

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -1,7 +1,7 @@
-/**
- * Provide the solution to LeetCode 3146 here:
- * https://leetcode.com/problems/permutation-difference-between-two-strings
- */
 export function findPermutationDifference(s: string, t: string): number {
-  return 0;
+  const idx = new Map<string, number>();
+  for (let i = 0; i < s.length; i++) idx.set(s[i], i);
+  let sum = 0;
+  for (let i = 0; i < t.length; i++) sum += Math.abs(i - (idx.get(t[i])!));
+  return sum;
 }
