a81e780f03700e20635220ac9423c7dcdfd017ef
Daniel Boyce <danielboyce20@gmail.com>
2025-09-09 14:15:05 +0000

    test:adds info to jest config to get code coverage

--- lesson_15/daniel-libraries-test/libraries/jest.config.js
+++ lesson_15/daniel-libraries-test/libraries/jest.config.js
@@ -8,4 +8,22 @@ export default {
     '^(\\.\\.?\\/.+)\\.js$': '$1',
   },
   extensionsToTreatAsEsm: ['.ts'],
+  preset: 'ts-jest',
+  collectCoverage: true, // Enable code coverage collection
+  coverageDirectory: 'coverage', // Directory to output coverage reports
+  collectCoverageFrom: [ // Specify files to include in coverage analysis
+    'src/media_collection.ts',
+     // Exclude declaration files
+  ],
+  // Optional: Configure thresholds for coverage
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
+
+
