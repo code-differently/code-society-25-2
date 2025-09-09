087d8ff100d17ff7a59e29b2951f0f06bb64400f
brooklynharden <hardenbrooklyn123@gmail.com>
2025-09-09 15:14:27 +0000

    feat:finished testing lesson15

--- lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Lesson15Test.java
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Lesson15Test.java
@@ -1,19 +1,35 @@
 package com.codedifferently.lesson15;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 class Lesson15Test {
 
   @Test
   public void testLesson15() {
-    assertThat(new Lesson15()).isNotNull();
+    //Arrange + Act
+    Lesson15 lesson15 = new Lesson15();
+    //Assert
+    assertThat(lesson15).isNotNull();
   }
 
   @Test
   public void testGetGreeting() {
+    // Arrange
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    PrintStream printStream = new PrintStream(outputStream);
+    PrintStream originalOut = System.out;
+    System.setOut(printStream);
     // Act
     Lesson15.main(null);
+    //Restore
+    System.setOut(originalOut);
+    // Assert
+    String expected = outputStream.toString().trim();
+    assertThat(expected).isEqualTo("Hello, World!");
+
   }
 }
