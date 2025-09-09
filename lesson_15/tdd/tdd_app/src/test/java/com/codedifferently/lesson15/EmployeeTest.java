e7b5379b790715c82465657181dd0c7a6ccaf3f1
brooklynharden <hardenbrooklyn123@gmail.com>
2025-09-09 15:53:25 +0000

    feat: created file for EmployeeTest and my getIdTest passed

---
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeTest.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.codedifferently.lesson15;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author vscode
+ */
+public class EmployeeTest {
+    private Employee employee;
+
+    @BeforeEach
+    public void setUp() {
+        employee = new Employee(7, "Brooklyn", "potatoes", 10000.00);
+    }
+    
+    
+    @Test
+    public void getIdTest(){
+        int expected = 7;
+        assertEquals(employee.getId(), expected);
+
+    }
+}
+
