f635a7427a1d5b1c50fe05ae8c532acb59899540
brooklynharden <hardenbrooklyn123@gmail.com>
2025-09-09 22:08:39 +0000

    feat:created a file for employeeManagerTest and addEmployeeTest passed

---
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeManagerTest.java
@@ -0,0 +1,33 @@
+package com.codedifferently.lesson15;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author vscode
+ */
+
+public class EmployeeManagerTest {
+    private Employee employee;
+    private EmployeeManager employeeManager;
+
+
+    @BeforeEach
+    public void setUp() {
+        employee = new Employee(7, "Brooklyn", "Potatoes", 10000.00);
+        employeeManager = new EmployeeManager();
+    }
+
+    @Test
+    public void addEmployeeTest(){
+        //adds the employee
+        employeeManager.addEmployee(employee);
+        //checks if the id matches the employee manager id
+        assertEquals(employeeManager.getEmployee(7), employee);
+    }
+
+
+}
