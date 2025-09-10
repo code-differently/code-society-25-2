c0b7641e36605d250bfeb505b77f32743f0e6f8a
Martha <iofuani252@gmail.com>
2025-09-10 19:01:14 +0000

    feat: implements addEmployeeTest unit test

---
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeManagerTest.java
@@ -0,0 +1,30 @@
+package com.codedifferently.lesson15;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import com.codedifferently.lesson15.Employee;
+import com.codedifferently.lesson15.EmployeeManager;
+
+
+
+public class EmployeeManagerTest {
+    private EmployeeManager employeeManager;
+    private Employee employee;
+
+    @BeforeEach
+    public void setUp() {
+        employeeManager = new EmployeeManager();
+        employee = new Employee(2, "Jane Austen", "Design", 75000, "UX Designer", 5);
+        employeeManager.addEmployee(employee);
+}
+
+    @Test 
+    public void addEmployeeTest() {
+        Employee employee = new Employee(1, "Adam Warlok", "Engineering", 100000, "Software Engineer", 3);
+        employeeManager.addEmployee(employee);
+        assertEquals(2, employeeManager.getEmployeeCount());
+    }
+
+}
