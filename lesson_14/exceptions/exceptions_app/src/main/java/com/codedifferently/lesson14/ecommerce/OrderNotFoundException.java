c9d141475f693366c3850cbd00299708d7de938c
calvin <cwrobinson252@mail.com>
2025-09-10 18:11:56 +0000

    Revert "feat: modified java files for exceptions handling"
    This reverts commit 33fdc6741253570f88b015d00643a4ffed134388.

--- lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/OrderNotFoundException.java
+++ lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/OrderNotFoundException.java
@@ -1,7 +1,8 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
 package com.codedifferently.lesson14.ecommerce;
 
-public class OrderNotFoundException extends Exception {
-  public OrderNotFoundException(String message) {
-    super(message);
-  }
-}
+class OrderNotFoundException {}
