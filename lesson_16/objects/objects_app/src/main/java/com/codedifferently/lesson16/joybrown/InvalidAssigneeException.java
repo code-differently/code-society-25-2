5a637709fa1c0eb9737b96ac2d6ff8e1907aa135
Joy Brown <jxbrown252@gmail.com>
2025-09-11 14:30:37 +0000

    feat(lesson16): add custom InvalidAssigneeException

---
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/joybrown/InvalidAssigneeException.java
@@ -0,0 +1,7 @@
+package com.codedifferently.lesson16.joybrown;
+
+public class InvalidAssigneeException extends RuntimeException {
+  public InvalidAssigneeException(String message) {
+    super(message);
+  }
+}
