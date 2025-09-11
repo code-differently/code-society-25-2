2f78114f259105bdc482cf1086441e5aacea6916
Daniel Boyce <danielboyce20@gmail.com>
2025-09-11 18:48:20 +0000

    feat:creates test file and base class

---
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/danielcustomobject/Home.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.codedifferently.lesson16.danielcustomobject;
+
+import java.util.List;
+
+/**
+ *
+ * @author vscode
+ */
+public class Home {
+
+    
+    private List<String> rooms;
+    private int stories;
+
+
+}
