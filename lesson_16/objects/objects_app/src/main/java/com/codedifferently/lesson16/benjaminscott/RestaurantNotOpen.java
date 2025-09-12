3cd94930c827cc9c4f33f9a8b0c69f6859deea5e
bscott519 <bbscott252@gmail.com>
2025-09-12 18:27:47 +0000

    Ran './gradlew :objects_app:spotlessApply' to fix errors in github again

--- lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/benjaminscott/RestaurantNotOpen.java
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/benjaminscott/RestaurantNotOpen.java
@@ -3,6 +3,5 @@ package com.codedifferently.lesson16.benjaminscott;
 public class RestaurantNotOpen extends RuntimeException {
   public RestaurantNotOpen(String message) {
     super(message);
-    
   }
 }
