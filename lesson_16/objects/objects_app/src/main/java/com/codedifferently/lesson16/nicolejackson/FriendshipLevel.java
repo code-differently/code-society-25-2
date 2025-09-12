4bb56ebe5039c9a76a4cf8f10f36b81f68b27b80
NcoleJac <ndjackson@gmail.com>
2025-09-12 21:07:44 +0000

    feat: addedBestFriend.java, BestFriendTest.java, FriendshipLevel.java, and InvalidActivityException.java and test passed for get name

---
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/nicolejackson/FriendshipLevel.java
@@ -0,0 +1,9 @@
+package com.codedifferently.lesson16.nicolejackson;
+public enum FriendshipLevel {
+ ACQUAINTANCE,
+    FRIEND,
+    CLOSE_FRIEND,
+    BEST_FRIEND,
+    FAMILY
+}
+
---
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/nicolejackson/InvalidActivityException.java
@@ -0,0 +1,8 @@
+package com.codedifferently.lesson16.nicolejackson;
+public class InvalidActivityException extends Exception {
+    public InvalidActivityException(String message) {
+        super(message);
+    }
+}
+
+
