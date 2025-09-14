e3e4a6b412e2a5ed14fd55e491008cc0510fa9b0
Martha <iofuani252@gmail.com>
2025-09-14 02:18:41 +0000

    feat: runs spotlessApply for all files

--- lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/marthao/CuisineType.java
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/marthao/CuisineType.java
@@ -1,11 +1,11 @@
 package com.codedifferently.lesson16.marthao;
 
 public enum CuisineType {
-    NIGERIAN,
-    ITALIAN,
-    GHANAIAN,
-    CHINESE,
-    JAPANESE,
-    MEXICAN,
-    AMERICAN
+  NIGERIAN,
+  ITALIAN,
+  GHANAIAN,
+  CHINESE,
+  JAPANESE,
+  MEXICAN,
+  AMERICAN
 }
--- lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/marthaotests/RestaurantTest.java
+++ lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/marthaotests/RestaurantTest.java
@@ -1,96 +1,97 @@
 package com.codedifferently.lesson16.marthaotests;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import com.codedifferently.lesson16.marthao.CuisineType;
 import com.codedifferently.lesson16.marthao.Restaurant;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class RestaurantTest {
-    private Restaurant restaurant;
+  private Restaurant restaurant;
 
-    @BeforeEach
-    public void setUp() {
-        restaurant = new Restaurant("Martha's Kitchen", 5.0, CuisineType.NIGERIAN, "123 Apple Street", new String[] {"Dinner"}); 
-    }
+  @BeforeEach
+  public void setUp() {
+    restaurant =
+        new Restaurant(
+            "Martha's Kitchen",
+            5.0,
+            CuisineType.NIGERIAN,
+            "123 Apple Street",
+            new String[] {"Dinner"});
+  }
 
-    @Test 
-    public void testGetName() {
-        //Arrange
-        String expected = "Martha's Kitchen";
-        //Act
-        String actual = restaurant.getName();
-        //Assert
-        assertEquals(expected, actual);
-    }
+  @Test
+  public void testGetName() {
+    // Arrange
+    String expected = "Martha's Kitchen";
+    // Act
+    String actual = restaurant.getName();
+    // Assert
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void testSetName() {
-        //Arrange
-        String expected = "Joe's Diner";
-        //Act
-        restaurant.setName("Joe's Diner");
-        String actual = restaurant.getName();
-        //Assert
-        assertEquals(expected, actual);
-    }
+  @Test
+  public void testSetName() {
+    // Arrange
+    String expected = "Joe's Diner";
+    // Act
+    restaurant.setName("Joe's Diner");
+    String actual = restaurant.getName();
+    // Assert
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void testGetRating() {
-        //Arrange
-        double expected = 5.0;
-        //Act
-        double actual = restaurant.getRating();
-        //Assert
-        assertEquals(expected, actual);
-    }
+  @Test
+  public void testGetRating() {
+    // Arrange
+    double expected = 5.0;
+    // Act
+    double actual = restaurant.getRating();
+    // Assert
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void testSetRating() {
-        //Arrange
-        double expected = 4.5;
-        //Act
-        restaurant.setRating(4.5);
-        double actual = restaurant.getRating();
-        //Assert
-        assertEquals(expected, actual);
-    }
+  @Test
+  public void testSetRating() {
+    // Arrange
+    double expected = 4.5;
+    // Act
+    restaurant.setRating(4.5);
+    double actual = restaurant.getRating();
+    // Assert
+    assertEquals(expected, actual);
+  }
 
-    @Test 
-    public void testGetCuisineType() {
-        //Arrange
-        CuisineType expected = CuisineType.NIGERIAN;
-        //Act
-        CuisineType actual = restaurant.getCuisineType();
-        //Assert
-        assertEquals(expected, actual);
-    }
+  @Test
+  public void testGetCuisineType() {
+    // Arrange
+    CuisineType expected = CuisineType.NIGERIAN;
+    // Act
+    CuisineType actual = restaurant.getCuisineType();
+    // Assert
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void testSetCuisineType() {
-        //Arrange
-        CuisineType expected = CuisineType.ITALIAN;
-        //Act
-        restaurant.setCuisineType(CuisineType.ITALIAN);
-        CuisineType actual = restaurant.getCuisineType();
-        //Assert
-        assertEquals(expected, actual);
-    }
+  @Test
+  public void testSetCuisineType() {
+    // Arrange
+    CuisineType expected = CuisineType.ITALIAN;
+    // Act
+    restaurant.setCuisineType(CuisineType.ITALIAN);
+    CuisineType actual = restaurant.getCuisineType();
+    // Assert
+    assertEquals(expected, actual);
+  }
 
-   @Test
-    public void testGetMenuByTime_Success() throws Exception {
-    //Arrange
+  @Test
+  public void testGetMenuByTime_Success() throws Exception {
+    // Arrange
     String[] menus = {"Breakfast", "Lunch", "Dinner"};
     restaurant = new Restaurant("Bob's Burgers", 4.0, CuisineType.AMERICAN, "123 Rd", menus);
-    //Act
+    // Act
     String actual = restaurant.getMenuByTime("Afternoon");
-    //Assert
+    // Assert
     assertEquals("Lunch", actual);
-    }
-
-  
+  }
 }
