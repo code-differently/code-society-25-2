4ff018cbed2eac201438d9b227c21bc555e629ee
DaFDE31 <adjocydanielson@gmail.com>
2025-09-15 14:51:23 +0000

    fix: fixed a moneyorder bug

--- lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/MoneyOrder.java
+++ lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/MoneyOrder.java
@@ -1,9 +1,9 @@
 package com.codedifferently.lesson17.bank;
 
 public class MoneyOrder extends Check {
-    public MoneyOrder(String checkNumber, double amount, Account account) {
-        super(checkNumber, amount, account);
-        depositFunds(account);
+    public MoneyOrder(String checkNumber, double amount, Account fromAccount, Account toAccount) {
+        super(checkNumber, amount, fromAccount);
+        depositFunds(toAccount);
         
     }
 }
