d22671ccf298ff39592e07360d1beade0a3d450f
Martha <iofuani252@gmail.com>
2025-09-17 22:36:50 +0000

    feat: add savings account files

---
+++ lesson_17/bank/bank_app/src/test/java/com/codedifferently/lesson17/bank/SavingsAccountTest.java
@@ -0,0 +1,36 @@
+package com.codedifferently.lesson17.bank;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.codedifferently.lesson17.bank.exceptions.InsufficientFundsException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SavingsAccountTest {
+    
+    private SavingsAccount classUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        classUnderTest = new SavingsAccount("987654321", 200.0);
+    }
+
+    @Test
+    void writeCheckThrowsException() {
+        assertThatExceptionOfType(UnsupportedOperationException.class)
+            .isThrownBy(() -> classUnderTest.writeCheck(50.0))
+            .withMessage("Cannot write checks from a savings account");
+    }
+
+    @Test
+    void testSavingsAccountCreation() {
+        assertEquals("987654321", classUnderTest.getAccountNumber());
+        assertEquals(200.0, classUnderTest.getBalance());
+    }
+}
