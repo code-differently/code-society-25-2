d76869040f6cf22f69ccc4310e14fac366626c14
brooklynharden <hardenbrooklyn123@gmail.com>
2025-09-17 15:16:40 +0000

    feat:test passed for testLogEntries

--- lesson_17/bank/bank_app/src/test/java/com/codedifferently/lesson17/bank/AuditLogTest.java
+++ lesson_17/bank/bank_app/src/test/java/com/codedifferently/lesson17/bank/AuditLogTest.java
@@ -32,4 +32,20 @@ public class AuditLogTest {
         assert(auditLog.getLogEntries().contains(logEntry1));
         assert(auditLog.getLogEntries().contains(logEntry2));
     }
+
+    @Test
+    void testLogEntries(){
+        // Arrange
+        String logEntry1 = "Customer " + customerIdOne + " deposited $200.00";
+        String logEntry2 = "Customer " + customerIdTwo + " withdrew $75.00";
+
+        // Act
+        auditLog.addToList(logEntry1);
+        auditLog.addToList(logEntry2);
+
+        // Assert
+        assert(auditLog.getLogSize() == 2);
+        assert(auditLog.getLogEntries().get(0).equals(logEntry1));
+        assert(auditLog.getLogEntries().get(1).equals(logEntry2));
+    }
 }
