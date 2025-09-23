c225c15b368c1e8127ff6df778e5e91b7596baff
ethanhillman <echillman252@gmail.com>
2025-09-23 19:58:45 +0000

    feat: add exception handling for product and order not found scenarios

--- lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/ProductNotFoundException.java
+++ lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/ProductNotFoundException.java
@@ -1,11 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+package com.codedifferently.lesson14.ecommerce; // Change this line
 
-package com.codedifferently.lesson14.ecommerce;
-
-class ProductNotFoundException extends Exception {
+public class ProductNotFoundException extends Exception {
   public ProductNotFoundException(String message) {
     super(message);
   }
