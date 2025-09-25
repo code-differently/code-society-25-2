85049215b70d17dc89dac925254bcec43b290316
jaydenellis <jlellis252@gmail.com>
2025-09-25 14:37:57 +0000

    folder name and package hotfix

---
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/f1drivers/F1Driver.java
@@ -0,0 +1,90 @@
+package com.codedifferently.lesson16.f1drivers;
+
+import java.util.ArrayList;
+
+enum Team {
+  MERCEDES,
+  FERRARI,
+  RED_BULL,
+  MCLAREN,
+  ASTON_MARTIN
+}
+
+class InvalidDriverAgeException extends Exception {
+  public InvalidDriverAgeException(String message) {
+    super(message);
+  }
+}
+
+public class F1Driver {
+  private String name;
+  private int age;
+  private Team team;
+  private ArrayList<String> raceWins;
+  private boolean worldChampion;
+  private int careerPoints;
+  private int podiums;
+
+  public F1Driver(
+      String name, int age, Team team, boolean worldChampion, int careerPoints, int podiums)
+      throws InvalidDriverAgeException {
+    if (age < 18) {
+      throw new InvalidDriverAgeException("F1 drivers must be at least 18 years old.");
+    }
+    this.name = name;
+    this.age = age;
+    this.team = team;
+    this.worldChampion = worldChampion;
+    this.careerPoints = careerPoints;
+    this.podiums = podiums;
+    this.raceWins = new ArrayList<>();
+  }
+
+  public boolean isVeteran() {
+    return this.age > 30;
+  }
+
+  public void addRaceWin(String raceName) {
+    raceWins.add(raceName);
+  }
+
+  public void printRaceWins() {
+    if (raceWins.isEmpty()) {
+      System.out.println(name + " has no race wins yet.");
+    } else {
+      System.out.println(name + "'s race wins:");
+      for (String win : raceWins) {
+        System.out.println("- " + win);
+      }
+    }
+  }
+
+  public void addPodium(int pointsEarned) {
+    podiums++;
+    careerPoints += pointsEarned;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public Team getTeam() {
+    return team;
+  }
+
+  public boolean isWorldChampion() {
+    return worldChampion;
+  }
+
+  public int getCareerPoints() {
+    return careerPoints;
+  }
+
+  public int getPodiums() {
+    return podiums;
+  }
+}
