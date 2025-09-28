6a69aaddb6f8122d8499907bec4fad59c80d1bc8
brooklynharden <hardenbrooklyn123@gmail.com>
2025-09-28 23:53:29 +0000

    feat:finished accordion component

--- lesson_20/brooklynharden/index.js
+++ lesson_20/brooklynharden/index.js
@@ -1,3 +1,4 @@
+//Tab
 function openTab(event, tabName){
   //Hide tab content
   const tabContent = document.getElementsByClassName("tabcontent");
@@ -5,14 +6,26 @@ function openTab(event, tabName){
     tabContent[i].style.display = "none";
   }
 
-  //Remove actice class from tab buttons
+  //Remove active class from tab buttons
   const tabLinks = document.getElementsByClassName("tablinks");
   for(let i=0;i<tabLinks.length;i++){
     tabLinks[i].classList.remove("active");
   }
 
-  
+
   document.getElementById(tabName).style.display = "block";
 
   event.currentTarget.classList.add("active");
+}
+
+//Accordion
+const accord = document.getElementsByClassName("accordion");
+for (let i = 0; i < accord.length; i++) {
+  accord[i].addEventListener("click", (event) => {
+    const accordionButton = event.currentTarget;
+    accordionButton.classList.toggle("active");
+   
+    const panel = accordionButton.nextElementSibling;
+    panel.classList.toggle("show");
+  });
 }
