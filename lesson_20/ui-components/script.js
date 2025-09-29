0b43c855770777dcc2c5c35e29877e39feb07485
Kerry Ferguson <kferg2021@gmail.com>
2025-09-29 17:46:52 +0000

    feat: adds image folder with images for offline capabilities. changes references to images in js file

--- lesson_20/ui-components/script.js
+++ lesson_20/ui-components/script.js
@@ -30,11 +30,11 @@ accHeaders.forEach(header => {
 
 // Gallery
 const images = [
-  'https://static0.thegamerimages.com/wordpress/wp-content/uploads/2020/08/Snowy1.jpg?q=50&fit=crop&w=825&dpr=1.5',
-  'https://i.pinimg.com/736x/e7/24/f6/e724f64f6d43679caa5365a0fda2abb3.jpg',
-  'https://64.media.tumblr.com/eef490213b5abeef75f360fb20c512d1/ae7f4a36cc63afbc-34/s250x400/fc6e299d1b0b3919f1144ed81431282c66b1148b.gif',
-  'https://i.pinimg.com/736x/d0/71/d2/d071d2cb8c8d9538151f86680b624d21.jpg',
-  'https://pa1.aminoapps.com/6665/1fa7477dbe2e18b9159e8c5f6f56f195b5a46db3_hq.gif'
+  'images/vulpix1.avif',
+  'images/vulpix2.jpg',
+  'images/vulpix3.gif',
+  'images/vulpix4.jpg',
+  'images/vulpix5.gif'
 ];
 let startIndex = 0;
 const gallery = document.getElementById('gallery');
