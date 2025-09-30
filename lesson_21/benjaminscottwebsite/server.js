b84815666bf72d0ff45569a8941244c2fe3b1721
bscott519 <bbscott252@gmail.com>
2025-09-30 19:49:55 +0000

    server is working

--- lesson_21/benjaminscottwebsite/server.js
+++ lesson_21/benjaminscottwebsite/server.js
@@ -1,4 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+var debug = require('debug')('myapp:server');
+
 const app = express()
 
-app.listen(3000)
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+ 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+ debug(`Server listening on http://localhost:${PORT}`);
+});
