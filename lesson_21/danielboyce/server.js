6b25362ff6bc32e1412c9bcb3209a5cd4df5db93
Daniel Boyce <danielboyce20@gmail.com>
2025-10-01 00:33:54 +0000

    feat:adds css to contact info form

--- lesson_21/danielboyce/server.js
+++ lesson_21/danielboyce/server.js
@@ -1,18 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.set("view engine","ejs")
-app.use(express.static("public"))
-app.get('/new', (req, res) => {
-  res.render("user-input")
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.get("/new", (req, res) => {
+  res.render("user-input");
 });
 
+app.post("/", (req, res) => {});
 
-app.post("/" , (req,res)=> {
-  
-
-})
-
-
-app.listen(PORT, () => {})
+app.listen(PORT, () => {});
