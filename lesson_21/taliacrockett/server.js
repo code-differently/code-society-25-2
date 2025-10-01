77bc7b62edd8ff658fd25bdcea6843b861f6163d
Talia Crockett <tmcrockett252@gmail.com>
2025-10-01 15:52:40 +0000

    feat: create web server with Express.js for lesson 21 assignment

---
+++ lesson_21/taliacrockett/server.js
@@ -0,0 +1,20 @@
+const express = require('express') 
+const app = express()
+const port = 4001
+const cors = require('cors')
+const path = require('path')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+// Serve static files (CSS, images, etc.)
+app.use(express.static(__dirname))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+})
