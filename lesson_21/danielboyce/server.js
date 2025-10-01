66c84abcb8f0a11b23072a1bf540e3b6ade592bb
Daniel Boyce <danielboyce20@gmail.com>
2025-10-01 14:22:02 +0000

    feat:adds data to render path to pass varaibles to user-view

--- lesson_21/danielboyce/server.js
+++ lesson_21/danielboyce/server.js
@@ -17,21 +17,24 @@ app.get("/new", (req, res) => {
 
 app.post("/new", (req, res) => {
   const user = req.body;
-  console.log({
-    "First Name":user.firstName,
-    "Last Name":user.lastName,
-    "Email":user.email,
-    "Phone Number":user.phone
+  // console.log({
+  //   "First Name":user.firstName,
+  //   "Last Name":user.lastName,
+  //   "Email":user.email,
+  //   "Phone Number":user.phone
 
-  })
+  // })
   req.session.user = user;
   res.redirect("/info");
 });
 
 app.get("/info",(req,res)=> {
-  const data = req.session.user
+  const data = req.session.user;
+  if(!data) {
+    return res.redirect("/new");
+  }
   
-  res.render('user-view');
+  res.render('user-view', data);
 })
 
 
