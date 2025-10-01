3264a5d9da77332a51b40dca4a946c0d8a2b59c4
brooklynharden <hardenbrooklyn123@gmail.com>
2025-10-01 15:25:34 +0000

    feat:making files and folders and wip express

---
+++ lesson_21/brooklynharden/server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app = express()
+
+app.set('view engine')
+app.get('/', (req, res) => {
+    console.log("Here")
+    res.render('index')
+  
+})
+app.listen(3000);
