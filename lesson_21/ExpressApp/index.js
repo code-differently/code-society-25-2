8224c1e0605a6eb51f5cbe0ea844f8929387f7cf
calvin <cwrobinson252@mail.com>
2025-10-01 15:19:26 +0000

    feat: created Express JS dir and files
    Signed-off-by: calvin <cwrobinson252@mail.com>

---
+++ lesson_21/ExpressApp/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors');
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
+
+app.get('/', (req, res) => {
+    res.send('This is working')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+
+})
