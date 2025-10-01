1e5eaaeb6b5e8b29337b0f81650cc69f4b74d67e
calvin <cwrobinson252@mail.com>
2025-10-01 16:49:21 +0000

    feat: created and modified contact form and js file
    Signed-off-by: calvin <cwrobinson252@mail.com>

--- lesson_21/ExpressApp/index.js
+++ lesson_21/ExpressApp/index.js
@@ -6,11 +6,19 @@ const cors = require('cors');
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(cors()); // Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+app.post('/contact', (req, res) => {
+  console.log(req.body);
+  res.json({ success: true });
+});
 
 app.get('/', (req, res) => {
-    res.send('This is working')
-})
-
+    res.send('This is working');
+});
 
 
 app.listen(port, () => {
