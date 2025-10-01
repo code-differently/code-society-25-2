3111489f612b52eade4f8dfd02559b4e4f09d256
Jared Edge <jarededgewrk@gmail.com>
2025-10-01 14:16:02 +0000

    feat: adds initial scaffold: package.json and server.js

---
+++ lesson_21/server.js
@@ -0,0 +1,45 @@
+
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+const PORT = 4000;
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+// parsing for form data 
+app.use(express.urlencoded({ extended: true }));
+// parsing for JSON bodies 
+app.use(express.json());
+
+
+//(homepage)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+
+app.post('/contact', (req, res) => {
+  const { name, email, message } = req.body;
+  res.send(`
+    <h1>Thank you, ${name}!</h1>
+    <p>We received your message:</p>
+    <ul>
+      <li><strong>Email:</strong> ${email}</li>
+      <li><strong>Message:</strong> ${message}</li>
+    </ul>
+    <a href="/">Back to Home</a>
+  `);
+});
+
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
