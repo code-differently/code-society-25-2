9a44825ed553a93ccf27b61efd41b8279279927c
brooklynharden <hardenbrooklyn123@gmail.com>
2025-10-01 19:43:42 +0000

    feat:completed homework

--- lesson_21/brooklynharden/server.js
+++ lesson_21/brooklynharden/server.js
@@ -14,15 +14,8 @@ app.get('/contact',(req, res) => {
     console.log("Hi")
     res.render('contact')
 }).post('/contact', (req, res)=>{
-    const user = req.body;
-    console.log({
-        "First Name:": user.firstName,
-        "Last Name:" : user.lastName,
-        "Email Address:": user.email,
-        "Phone Number:":user.phone
-    })
-    req.sesson.user = user;
-    res.redirect("/info");
+    console.log(req.body.firstname)
+    res.render('displayInfo',req.body)
 })
 
 
