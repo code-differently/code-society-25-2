7b0c0a775ce4d89a3df0a6f8b8cf8d65b80e3175
brooklynharden <hardenbrooklyn123@gmail.com>
2025-10-01 16:03:15 +0000

    refactor: form is establish for site but working on submission

--- lesson_21/brooklynharden/server.js
+++ lesson_21/brooklynharden/server.js
@@ -2,10 +2,16 @@ const express = require("express")
 
 const app = express()
 app.use(express.static("public"))
-app.set('view engine')
+app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
     console.log("Here")
     res.render('index')
   
 })
+
+app.get('/contact',(req, res) => {
+    console.log("Hi")
+    res.render('contact')
+})
+
 app.listen(3000);
