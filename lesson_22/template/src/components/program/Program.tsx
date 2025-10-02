dcf61977fbccb735f4ca6139cd406728c1084e8b
Daniel Boyce <danielboyce20@gmail.com>
2025-10-02 13:46:38 +0000

    feat:adds program data and html to custom component

--- lesson_22/template/src/components/program/Program.tsx
+++ lesson_22/template/src/components/program/Program.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-
-const Program = FC<ProgramProps> ({title,descriptino}) => {
+import React, { FC, JSX } from 'react'
+import { ProgramProps } from '@/types/types'
+const Program :FC<ProgramProps>  = ({program}):JSX.Element => {
 
   return (
     <div>
-      
+      <h3>{program.title}</h3>
+            <p>
+              {program.description}
+            </p>
     </div>
   )
 }
