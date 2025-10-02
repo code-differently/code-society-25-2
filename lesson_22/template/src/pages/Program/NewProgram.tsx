c4b0ea35ab965713bfe8f18c2c650d3490e5e374
Daniel Boyce <danielboyce20@gmail.com>
2025-10-02 18:03:33 +0000

    feat:uses usenavigate hook in the onsubmit attribute of the form to navigate to home page

--- lesson_22/template/src/pages/Program/NewProgram.tsx
+++ lesson_22/template/src/pages/Program/NewProgram.tsx
@@ -20,19 +20,12 @@ const NewProgram = () => {
     const handleDescChange =  (event:React.ChangeEvent<HTMLInputElement>) => {
         setDescValue(event.target.value)
     }
-    const subimtNewProgram = () => {
-      const newProgram: ProgramType = {
-        title: titleValue,
-        description: descValue,
-      };
-      
-
-    };
+    
     
   return (
     <div>
       <h1>Add a new Program</h1>
-      <form onSubmit={}>
+      <form onSubmit={()=>navigate('/')}>
         <input type="text" name="title" id="" placeholder='Enter a title' value={titleValue} onChange={handleTitleChange}/>
         <input type="text" name="description" id="" placeholder='Enter a description' value={descValue} onChange={handleDescChange}/>
         <button type='submit' onClick={subimtNewProgram}>Submit</button>
