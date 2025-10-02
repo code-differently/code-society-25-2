525fa412f36b64e11d747dca01dba5766f5b73f6
Daniel Boyce <danielboyce20@gmail.com>
2025-10-02 13:42:10 +0000

    feat:creates type for props in program

---
+++ lesson_22/template/src/components/program/Program.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Program = ({title,descriptino}) => {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Program
---
+++ lesson_22/template/src/types/types.ts
@@ -0,0 +1,6 @@
+export type ProgramType = {
+    title:string,
+    description:string
+}
+
+export interface 
