9c19939330c63bedcbf2b893a97b412ea52dcdac
Daniel Boyce <danielboyce20@gmail.com>
2025-10-02 14:41:40 +0000

    feat:adds logic to add new input into input text

--- lesson_22/template/src/pages/Program/NewProgram.tsx
+++ lesson_22/template/src/pages/Program/NewProgram.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
+import ProgramList from '@/components/program/Program'
 
 const NewProgram = () => {
+    const [titleValue,setTitleValue] = useState('');
+    const [descValue,setDescValue] = useState('');
+    const handleChange = (event:React.ChangeEvent<HTMLInputElement>)=> {
+        setTitleValue(event.target.value);
+
+    }
+    const subimtNewProgram = ()=> {
+        newProgra
+    }
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Add a new Program</h1>
+      <form action="subimt">
+        <input type="text" name="title" id="" placeholder='Enter a title' value={titleValue} onChange={handleChange}/>
+        <input type="text" name="description" id="" placeholder='Enter a description' />
+        <button type='submit' onClick={}>Submit</button>
+      </form>
     </div>
   )
 }
