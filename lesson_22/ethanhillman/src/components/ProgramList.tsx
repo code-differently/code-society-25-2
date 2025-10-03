3344cd6f622836afb7e553e9e3341a07d3b53fc0
ethanhillman <echillman252@gmail.com>
2025-10-03 05:27:18 +0000

    feat: refactor ProgramList to use ProgramComponent for program details

--- lesson_22/ethanhillman/src/components/ProgramList.tsx
+++ lesson_22/ethanhillman/src/components/ProgramList.tsx
@@ -1,58 +1,50 @@
-import Program from "./Program";
+
+type ProgramProps = {
+  title: string;
+  description: string;
+};
+
+function ProgramComponent({ title, description }: ProgramProps) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
 
 export default function ProgramList() {
-    return(
-        <section className="programs-section">
-        <h2>
-          Our <em className="highlight">Programs</em>
-        </h2>
-        <ul className="programs">
-          <li className="program">
-            <Program
-              title="Swine Short Loin"
-              description="Swine short loin burgdoggen ball tip, shank ham hock bacon.
-                Picanha strip steak pork, swine boudin ham meatball meatloaf
-                leberkas sausage. Turkey beef andouille kielbasa rump pastrami
-                biltong chislic alcatra buffalo prosciutto jowl. Pastrami chicken
-                sirloin swine capicola landjaeger jowl boudin pork chop shankle
-                bresaola turducken leberkas ham."
-            />
-          </li>
-          <li className="program">
-            <Program
+  return (
+    <section className="program-section">
+      <h2>
+        Our <em className="highlight">Programs</em>
+      </h2>
+      <ul className="program-list">
+        <li className="program-item">
+          <ProgramComponent
+            title="Swine Short Loin"
+            description="Swine short loin burgdoggen ball tip, shank ham hock bacon. Picanha strip steak pork, swine boudin ham meatball meatloaf leberkas sausage. Turkey beef andouille kielbasa rump pastrami biltong chislic alcatra buffalo prosciutto jowl. Pastrami chicken sirloin swine capicola landjaeger jowl boudin pork chop shankle bresaola turducken leberkas ham."
+          />
+        </li>
+        <li className="program-item">
+          <ProgramComponent
             title="Bacon Ipsum"
-            description="Bacon ipsum dolor amet leberkas chuck biltong pork loin sirloin
-              bresaola rump frankfurter. Shoulder doner strip steak chuck. Short
-              ribs venison salami chuck sirloin jowl chislic cupim swine cow.
-              Corned beef chuck frankfurter tenderloin venison biltong andouille
-              leberkas kielbasa sausage t-bone turducken fatback. Pork picanha
-              buffalo bacon tail salami meatball, jowl chislic."
-              />
-          </li>
-          <li className="program">
-            <Program
+            description="Bacon ipsum dolor amet leberkas chuck biltong pork loin sirloin bresaola rump frankfurter. Shoulder doner strip steak chuck. Short ribs venison salami chuck sirloin jowl chislic cupim swine cow. Corned beef chuck frankfurter tenderloin venison biltong andouille leberkas kielbasa sausage t-bone turducken fatback. Pork picanha buffalo bacon tail salami meatball, jowl chislic."
+          />
+        </li>
+        <li className="program-item">
+          <ProgramComponent
             title="Picanha Swine Jowl"
-            description="Picanha swine jowl meatball boudin pastrami bresaola fatback
-              shankle pork belly cow. Frankfurter ground round shank corned beef
-              chuck. Jerky frankfurter fatback capicola hamburger, pork
-              prosciutto bresaola ham porchetta rump t-bone pancetta tenderloin.
-              Fatback ham hock prosciutto, tenderloin shoulder salami tri-tip
-              leberkas bacon turducken chislic venison sausage frankfurter."
-            />
-          </li>
-          <li className="program">
-            <Program
+            description="Picanha swine jowl meatball boudin pastrami bresaola fatback shankle pork belly cow. Frankfurter ground round shank corned beef chuck. Jerky frankfurter fatback capicola hamburger, pork prosciutto bresaola ham porchetta rump t-bone pancetta tenderloin. Fatback ham hock prosciutto, tenderloin shoulder salami tri-tip leberkas bacon turducken chislic venison sausage frankfurter."
+          />
+        </li>
+        <li className="program-item">
+          <ProgramComponent
             title="Kevin Chicken T-Bone"
-            description="Kevin chicken t-bone spare ribs shankle bacon drumstick kielbasa
-              cow jowl doner salami chuck andouille. Rump spare ribs bresaola
-              frankfurter shankle. Bresaola ribeye turducken, cow leberkas
-              boudin biltong sirloin filet mignon ham pork belly shank.
-              Tenderloin sirloin pancetta pork loin shankle venison turducken
-              boudin. Brisket tenderloin salami ham bresaola burgdoggen."
-            />
-          </li>
-        </ul>
-      </section>
-    );
-
+            description="Kevin chicken t-bone spare ribs shankle bacon drumstick kielbasa cow jowl doner salami chuck andouille. Rump spare ribs bresaola frankfurter shankle. Bresaola ribeye turducken, cow leberkas boudin biltong sirloin filet mignon ham pork belly shank. Tenderloin sirloin pancetta pork loin shankle venison turducken boudin. Brisket tenderloin salami ham bresaola burgdoggen."
+          />
+        </li>
+      </ul>
+    </section>
+  );
 }
