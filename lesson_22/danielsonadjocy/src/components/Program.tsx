5c0de8b9e0716bf473b8f2e83218c5a241fae6d0
Danielson A. <adjocydanielson@gmail.com>
2025-10-03 03:55:42 +0000

    fix: program paramters fixed; Map to loop through programs

--- lesson_22/danielsonadjocy/src/components/Program.tsx
+++ lesson_22/danielsonadjocy/src/components/Program.tsx
@@ -1,5 +1,5 @@
 
-export default function Program( title: string, description: string ) {
+export default function Program( {title, description} : {title: string, description: string} ) {
   return (
     <div className="program">
       <h3>{title}</h3>
