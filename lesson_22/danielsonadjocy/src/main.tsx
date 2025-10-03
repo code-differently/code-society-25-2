e8a76f51fb28f35b6126d82d851be5c3e3154d25
Danielson A. <adjocydanielson@gmail.com>
2025-10-03 14:39:34 +0000

    feat: ProgramForm communication working; using OutletContext

--- lesson_22/danielsonadjocy/src/main.tsx
+++ lesson_22/danielsonadjocy/src/main.tsx
@@ -1,5 +1,6 @@
 import App from './App.tsx';
 import {Home} from './pages/Home/Home.tsx';
+import {ProgramForm} from './pages/program_form/ProgramForm.tsx';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />,
+      },
+      {
+        path: '/programForm',
+        element: <ProgramForm />,
       },
     ],
   },
