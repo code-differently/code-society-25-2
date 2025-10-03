eaf81abef66c1670f969938d1b4c1c24c8d81a14
Anthony D. Mays <anthonydmays@users.noreply.github.com>
2025-10-02 21:23:19 -0400

    Use environment variable for server port

--- lesson_21/danielboyce/server.js
+++ lesson_21/danielboyce/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const session = require("express-session");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
