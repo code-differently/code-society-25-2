d56053030c89078e18deca8cc694b33369a2e867
brooklynharden <hardenbrooklyn123@gmail.com>
2025-10-03 18:00:18 +0000

    feat: render errors fix, working on fixing the  saving the new program data

--- lesson_22/brooklynharden/template/src/pages/NewProgram/NewProgram.tsx
+++ lesson_22/brooklynharden/template/src/pages/NewProgram/NewProgram.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const NewProgram = () => {
+export const NewProgram: React.FC = () => {
+    const [title, setTitle] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+   
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+    }
+
+    console.log('New Program Submitted: ', {title, description});
+
+    // setTitle('');
+    // setDescription('');
+
   return (
-    <div>NewProgram</div>
+    <section className="new-program">
+    <h1>Program Form</h1>
+    <form onSubmit={handleSubmit} className="new-program">
+   <div>
+        <label htmlFor="title">Program Title</label>
+        <input
+            id="title"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            />
+   </div>
+
+    <div>
+        <label htmlFor="description">Program Description</label>
+        <input
+            id="description"
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            />
+   </div>
+   
+        <button type="submit">Add Program</button>
+   </form>
+    </section>
+
+
   )
 }
