8bb284eb8d6b813f54ee5e4a645dd0921015d8c2
ethanhillman <echillman252@gmail.com>
2025-10-03 05:18:55 +0000

    feat: refactor ProgramList to use Program component for program details

--- lesson_22/ethanhillman/src/components/ProgramList.tsx
+++ lesson_22/ethanhillman/src/components/ProgramList.tsx
@@ -1,3 +1,5 @@
+import Program from "./Program";
+
 export default function ProgramList() {
     return(
         <section className="programs-section">
@@ -6,48 +8,48 @@ export default function ProgramList() {
         </h2>
         <ul className="programs">
           <li className="program">
-            <h3>Swine Short Loin</h3>
-            <p>
-              Swine short loin burgdoggen ball tip, shank ham hock bacon.
-              Picanha strip steak pork, swine boudin ham meatball meatloaf
-              leberkas sausage. Turkey beef andouille kielbasa rump pastrami
-              biltong chislic alcatra buffalo prosciutto jowl. Pastrami chicken
-              sirloin swine capicola landjaeger jowl boudin pork chop shankle
-              bresaola turducken leberkas ham.
-            </p>
+            <Program
+              title="Swine Short Loin"
+              description="Swine short loin burgdoggen ball tip, shank ham hock bacon.
+                Picanha strip steak pork, swine boudin ham meatball meatloaf
+                leberkas sausage. Turkey beef andouille kielbasa rump pastrami
+                biltong chislic alcatra buffalo prosciutto jowl. Pastrami chicken
+                sirloin swine capicola landjaeger jowl boudin pork chop shankle
+                bresaola turducken leberkas ham."
+            />
           </li>
           <li className="program">
-            <h3>Bacon Ipsum</h3>
-            <p>
-              Bacon ipsum dolor amet leberkas chuck biltong pork loin sirloin
+            <Program
+            title="Bacon Ipsum"
+            description="Bacon ipsum dolor amet leberkas chuck biltong pork loin sirloin
               bresaola rump frankfurter. Shoulder doner strip steak chuck. Short
               ribs venison salami chuck sirloin jowl chislic cupim swine cow.
               Corned beef chuck frankfurter tenderloin venison biltong andouille
               leberkas kielbasa sausage t-bone turducken fatback. Pork picanha
-              buffalo bacon tail salami meatball, jowl chislic.
-            </p>
+              buffalo bacon tail salami meatball, jowl chislic."
+              />
           </li>
           <li className="program">
-            <h3>Picanha Swine Jowl</h3>
-            <p>
-              Picanha swine jowl meatball boudin pastrami bresaola fatback
+            <Program
+            title="Picanha Swine Jowl"
+            description="Picanha swine jowl meatball boudin pastrami bresaola fatback
               shankle pork belly cow. Frankfurter ground round shank corned beef
               chuck. Jerky frankfurter fatback capicola hamburger, pork
               prosciutto bresaola ham porchetta rump t-bone pancetta tenderloin.
               Fatback ham hock prosciutto, tenderloin shoulder salami tri-tip
-              leberkas bacon turducken chislic venison sausage frankfurter.
-            </p>
+              leberkas bacon turducken chislic venison sausage frankfurter."
+            />
           </li>
           <li className="program">
-            <h3>Kevin Chicken T-Bone</h3>
-            <p>
-              Kevin chicken t-bone spare ribs shankle bacon drumstick kielbasa
+            <Program
+            title="Kevin Chicken T-Bone"
+            description="Kevin chicken t-bone spare ribs shankle bacon drumstick kielbasa
               cow jowl doner salami chuck andouille. Rump spare ribs bresaola
               frankfurter shankle. Bresaola ribeye turducken, cow leberkas
               boudin biltong sirloin filet mignon ham pork belly shank.
               Tenderloin sirloin pancetta pork loin shankle venison turducken
-              boudin. Brisket tenderloin salami ham bresaola burgdoggen.
-            </p>
+              boudin. Brisket tenderloin salami ham bresaola burgdoggen."
+            />
           </li>
         </ul>
       </section>
