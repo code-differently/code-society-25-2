bea57a32221e86ff5740796b68dc6d128e241f68
Tyran Rice <tyranricejr@gmail.com>
2025-10-05 22:10:58 +0000

    feat: adds "GET" request for media item ID

--- lesson_23/api/java/api_app/src/main/java/com/codedifferently/lesson23/web/MediaItemsController.java
+++ lesson_23/api/java/api_app/src/main/java/com/codedifferently/lesson23/web/MediaItemsController.java
@@ -1,16 +1,19 @@
 package com.codedifferently.lesson23.web;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.codedifferently.lesson23.library.Librarian;
 import com.codedifferently.lesson23.library.Library;
 import com.codedifferently.lesson23.library.MediaItem;
 import com.codedifferently.lesson23.library.search.SearchCriteria;
-import java.io.IOException;
-import java.util.List;
-import java.util.Set;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
@@ -29,6 +32,20 @@ public class MediaItemsController {
     Set<MediaItem> items = library.search(SearchCriteria.builder().build());
     List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
     var response = GetMediaItemsResponse.builder().items(responseItems).build();
+    return ResponseEntity.ok(response);
+  }
+
+  @GetMapping("/items/{id}")
+  public ResponseEntity<GetMediaItemsResponse> getItemsById(@PathVariable String id) {
+    SearchCriteria criteria = SearchCriteria.builder().id(id).build();
+    Set<MediaItem> items = library.search(criteria);
+    
+    if(items.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+
+    MediaItemResponse responseItem = MediaItemResponse.from(items.iterator().next());
+    var response = GetMediaItemsResponse.builder().item(responseItem).build();
     return ResponseEntity.ok(response);
   }
 }
