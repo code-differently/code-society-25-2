0619bd00f6cf00bd1120332f87e3c2a565079ef0
Kerry Ferguson <kferg2021@gmail.com>
2025-10-05 18:44:43 +0000

    feat: changes Home.tsx to actually use react components

--- lesson_22/kerryferguson/src/components/program-list/ProgramList.tsx
+++ lesson_22/kerryferguson/src/components/program-list/ProgramList.tsx
@@ -1,13 +1,18 @@
 import {Program, ProgramData} from '../program';
 
 export const ProgramList = ({programs}: {programs: ProgramData[]}) => (
-  <ul className="programs">
-    {programs.map(eachProgram => (
-      <Program
-        key={eachProgram.title}
-        title={eachProgram.title}
-        description={eachProgram.description}
-      />
-    ))}{' '}
-  </ul>
+  <section className="programs-section">
+    <h2>
+      Our <em className="highlight">Programs</em>
+    </h2>
+    <ul className="programs">
+      {programs.map(eachProgram => (
+        <Program
+          key={eachProgram.title}
+          title={eachProgram.title}
+          description={eachProgram.description}
+        />
+      ))}
+    </ul>
+  </section>
 );
