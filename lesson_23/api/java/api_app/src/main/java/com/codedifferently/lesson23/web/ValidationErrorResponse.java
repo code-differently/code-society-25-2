baaa6ce5bbdeecaf1876ae80c8438aba2409729b
jrcespedes252-hub <jrcespedes252@gmail.com>
2025-10-06 15:39:10 +0000

    eat: implement REST API endpoints for media items management

---
+++ lesson_23/api/java/api_app/src/main/java/com/codedifferently/lesson23/web/ValidationErrorResponse.java
@@ -0,0 +1,16 @@
+package com.codedifferently.lesson23.web;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ValidationErrorResponse {
+
+  private List<String> errors;
+}
