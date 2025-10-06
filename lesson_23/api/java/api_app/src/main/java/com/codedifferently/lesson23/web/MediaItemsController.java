b7a580994296ea59b63f950546d24371801c6902
jrcespedes252-hub <jrcespedes252@gmail.com>
2025-10-06 16:05:44 +0000

    feat: implement complete REST API for media items management

--- lesson_23/api/java/api_app/src/main/java/com/codedifferently/lesson23/web/MediaItemsController.java
+++ lesson_23/api/java/api_app/src/main/java/com/codedifferently/lesson23/web/MediaItemsController.java
@@ -1,15 +1,10 @@
 package com.codedifferently.lesson23.web;
 
-import com.codedifferently.lesson23.library.Librarian;
-import com.codedifferently.lesson23.library.Library;
-import com.codedifferently.lesson23.library.MediaItem;
-import com.codedifferently.lesson23.library.search.SearchCriteria;
-import jakarta.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -19,6 +14,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.codedifferently.lesson23.library.Librarian;
+import com.codedifferently.lesson23.library.Library;
+import com.codedifferently.lesson23.library.MediaItem;
+import com.codedifferently.lesson23.library.search.SearchCriteria;
+
+import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin
@@ -68,7 +70,7 @@ public class MediaItemsController {
     }
 
     try {
-      MediaItem item = MediaItemRequest.toMediaItem(request.getItem());
+      MediaItem item = MediaItemRequest.asMediaItem(request.getItem());
       library.addMediaItem(item, librarian);
       MediaItemResponse response = MediaItemResponse.from(item);
       return ResponseEntity.ok(CreateMediaItemResponse.builder().item(response).build());
