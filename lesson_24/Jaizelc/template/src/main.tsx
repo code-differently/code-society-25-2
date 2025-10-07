ff2067892ddd6687ada31fed3739c22fc578ee36
jrcespedes252-hub <jrcespedes252@gmail.com>
2025-10-07 20:34:20 +0000

    feat(lesson24): integrate API with ProgramList component and add program creation functionality

--- lesson_24/Jaizelc/template/src/main.tsx
+++ lesson_24/Jaizelc/template/src/main.tsx
@@ -1,6 +1,7 @@
 import App from './App.tsx';
 import {Home} from './pages/Home/Home.tsx';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 
