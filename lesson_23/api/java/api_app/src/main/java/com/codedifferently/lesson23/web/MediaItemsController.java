a9949dbfa64f75fa6e3f02bf1b076e739ff061c8
Talia Crockett <tmcrockett252@gmail.com>
2025-10-07 14:50:48 +0000

    fix: updates the addItem method for test to be successful

--- lesson_23/api/java/api_app/src/main/java/com/codedifferently/lesson23/web/MediaItemsController.java
+++ lesson_23/api/java/api_app/src/main/java/com/codedifferently/lesson23/web/MediaItemsController.java
@@ -1,10 +1,13 @@
 package com.codedifferently.lesson23.web;
 
+import com.codedifferently.lesson23.library.Librarian;
+import com.codedifferently.lesson23.library.Library;
+import com.codedifferently.lesson23.library.MediaItem;
+import com.codedifferently.lesson23.library.search.SearchCriteria;
+import jakarta.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,13 +16,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.codedifferently.lesson23.library.Librarian;
-import com.codedifferently.lesson23.library.Library;
-import com.codedifferently.lesson23.library.MediaItem;
-import com.codedifferently.lesson23.library.search.SearchCriteria;
-
-import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin
@@ -42,26 +38,26 @@ public class MediaItemsController {
   }
 
   @GetMapping("/items/{id}")
-  public ResponseEntity<GetMediaItemsResponse> getItems(@PathVariable String id) {
+  public ResponseEntity<GetMediaItemsResponse> getItem(@PathVariable String id) {
     Set<MediaItem> items = library.search(SearchCriteria.builder().id(id).build());
     if (items.isEmpty()) {
       return ResponseEntity.notFound().build();
     }
-    
+
     List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
     var response = GetMediaItemsResponse.builder().items(responseItems).build();
     return ResponseEntity.ok(response);
   }
 
   @PostMapping("/items")
-  public ResponseEntity<CreateMediaItemResponse> addItem(@Valid @RequestBody CreateMediaItemRequest request) {
+  public ResponseEntity<CreateMediaItemResponse> addItem(
+      @Valid @RequestBody CreateMediaItemRequest request) {
     MediaItem item = MediaItemRequest.asMediaItem(request.getItem());
     library.addMediaItem(item, librarian);
 
-    var createResponse = CreateMediaItemResponse.builder().item(MediaItemResponse.from(item)).build();
-    return ResponseEntity.status(HttpStatus.CREATED).body(createResponse);
+    var response = CreateMediaItemResponse.builder().item(MediaItemResponse.from(item)).build();
+    return ResponseEntity.ok(response);
   }
-  
 
   @DeleteMapping("/items/{id}")
   public ResponseEntity<Void> deleteItem(@PathVariable String id) {
