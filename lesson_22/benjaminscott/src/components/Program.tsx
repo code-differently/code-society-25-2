c7cf58d46ddebf91cc8b5021277602db735c6d2e
bscott519 <bbscott252@gmail.com>
2025-10-07 20:52:24 +0000

    adding program and programlist for reference for lesson 24

---
+++ lesson_22/benjaminscott/src/components/Program.tsx
@@ -0,0 +1,13 @@
+interface ProgramProps {
+  title: string;
+  description: string;
+}
+
+export default function Program({title, description}: ProgramProps) {
+  return (
+    <li className="program">
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
