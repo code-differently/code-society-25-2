a69c6a7fde30c69e2b6855daa68915533c8b42e5
brooklynharden <hardenbrooklyn123@gmail.com>
2025-10-08 12:58:20 +0000

    refactor: apply spotlessapply

--- lesson_23/api/java/api_app/src/main/java/com/codedifferently/lesson23/web/MediaItemsController.java
+++ lesson_23/api/java/api_app/src/main/java/com/codedifferently/lesson23/web/MediaItemsController.java
@@ -1,11 +1,15 @@
 package com.codedifferently.lesson23.web;
 
+import com.codedifferently.lesson23.library.Librarian;
+import com.codedifferently.lesson23.library.Library;
+import com.codedifferently.lesson23.library.MediaItem;
+import com.codedifferently.lesson23.library.search.SearchCriteria;
+import jakarta.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,13 +18,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.codedifferently.lesson23.library.Librarian;
-import com.codedifferently.lesson23.library.Library;
-import com.codedifferently.lesson23.library.MediaItem;
-import com.codedifferently.lesson23.library.search.SearchCriteria;
-
-import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin
@@ -41,33 +38,36 @@ public class MediaItemsController {
     var response = GetMediaItemsResponse.builder().items(responseItems).build();
     return ResponseEntity.ok(response);
   }
-  
+
   @GetMapping("/items/{id}")
-  public ResponseEntity<GetMediaItemsResponse> getItemID(@PathVariable String id){
+  public ResponseEntity<GetMediaItemsResponse> getItemID(@PathVariable String id) {
     Set<MediaItem> items = library.search(SearchCriteria.builder().id(id).build());
-    //Optional can be an  object or null ex. a:string | null;
-    //Optional here is saying if the first item ID is found grab it but if not keep it empty/null.
+    // Optional can be an  object or null ex. a:string | null;
+    // Optional here is saying if the first item ID is found grab it but if not keep it empty/null.
     Optional<MediaItem> itemsId = items.stream().findFirst();
     MediaItemResponse responseItem = null;
-    if(itemsId.isEmpty()){
+    if (itemsId.isEmpty()) {
       return ResponseEntity.notFound().build();
     }
-      responseItem = MediaItemResponse.from(itemsId.get());
-      var response = GetMediaItemsResponse.builder().item(responseItem).build();
-      return ResponseEntity.ok(response);
+    responseItem = MediaItemResponse.from(itemsId.get());
+    var response = GetMediaItemsResponse.builder().item(responseItem).build();
+    return ResponseEntity.ok(response);
   }
+
   @DeleteMapping("items/{id}")
-  public ResponseEntity<GetMediaItemsResponse> deleteItemId(@PathVariable String id){
+  public ResponseEntity<GetMediaItemsResponse> deleteItemId(@PathVariable String id) {
     Set<MediaItem> items = library.search(SearchCriteria.builder().id(id).build());
     Optional<MediaItem> itemsId = items.stream().findFirst();
-    if(itemsId.isPresent()){
+    if (itemsId.isPresent()) {
       library.removeMediaItem(itemsId.get(), librarian);
       return ResponseEntity.noContent().build();
     }
-      return ResponseEntity.notFound().build();
+    return ResponseEntity.notFound().build();
   }
- @PostMapping("/items")
-  public ResponseEntity<Map<String, MediaItemResponse>> addItem(@Valid @RequestBody CreateMediaItemRequest request) {
+
+  @PostMapping("/items")
+  public ResponseEntity<Map<String, MediaItemResponse>> addItem(
+      @Valid @RequestBody CreateMediaItemRequest request) {
     try {
       MediaItem items = MediaItemRequest.asMediaItem(request.getItem());
       library.addMediaItem(items, librarian);
@@ -79,6 +79,4 @@ public class MediaItemsController {
       return ResponseEntity.badRequest().build();
     }
   }
-
-
 }
