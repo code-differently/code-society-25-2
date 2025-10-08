a224eea0d9f70d7fa1c65f5685bc45bd93962b7e
Daniel Boyce <danielboyce20@gmail.com>
2025-10-08 12:32:42 +0000

    chore:restored template to base version without changes

--- lesson_22/template/src/main.tsx
+++ lesson_22/template/src/main.tsx
@@ -1,19 +1,11 @@
 import App from './App.tsx';
-import { Home } from './pages/Home/Home.tsx';
-import NewProgram from './pages/Program/NewProgram.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {Home} from './pages/Home/Home.tsx';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
-
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 
 import './index.scss';
-import ProgramInfo from './pages/ProgramInfo/ProgramInfo.tsx';
-
-
-
-
 
 const queryClient = new QueryClient();
 
@@ -26,14 +18,6 @@ const router = createBrowserRouter([
         path: '/',
         element: <Home />,
       },
-      {
-        path: '/new-program',
-        element: <NewProgram />,
-      },
-      {
-        path:"/:id",
-        element:<ProgramInfo/>
-      }
     ],
   },
 ]);
