950ee885b332db57a0be67fbf04e5bb5404c539f
Kerry Ferguson <kferg2021@gmail.com>
2025-10-08 03:34:28 +0000

    chore: fixes formatting using spotlessApply

--- lesson_23/api/java/api_app/src/main/java/com/codedifferently/lesson23/web/MediaItemsController.java
+++ lesson_23/api/java/api_app/src/main/java/com/codedifferently/lesson23/web/MediaItemsController.java
@@ -1,10 +1,14 @@
 package com.codedifferently.lesson23.web;
 
+import com.codedifferently.lesson23.library.Librarian;
+import com.codedifferently.lesson23.library.Library;
+import com.codedifferently.lesson23.library.MediaItem;
+import com.codedifferently.lesson23.library.search.SearchCriteria;
+import jakarta.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,14 +17,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.codedifferently.lesson23.library.Librarian;
-import com.codedifferently.lesson23.library.Library;
-import com.codedifferently.lesson23.library.MediaItem;
-import com.codedifferently.lesson23.library.search.SearchCriteria;
-
-import jakarta.validation.Valid;
-
 
 @RestController
 @CrossOrigin
@@ -40,10 +36,9 @@ public class MediaItemsController {
     List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
     var response = GetMediaItemsResponse.builder().items(responseItems).build();
     return ResponseEntity.ok(response);
-    
   }
 
-@GetMapping("/items/{id}")
+  @GetMapping("/items/{id}")
   public ResponseEntity<GetMediaItemsResponse> getItemsById(@PathVariable String id) {
     SearchCriteria getCriteria = SearchCriteria.builder().id(id).build();
     Set<MediaItem> items = library.search(getCriteria);
@@ -57,7 +52,7 @@ public class MediaItemsController {
     return ResponseEntity.ok(response);
   }
 
-@PostMapping("/items")
+  @PostMapping("/items")
   public ResponseEntity<Map<String, MediaItemResponse>> addItem(
       @Valid @RequestBody CreateMediaItemRequest request) {
     try {
@@ -72,7 +67,7 @@ public class MediaItemsController {
     }
   }
 
-@DeleteMapping("/items/{id}")
+  @DeleteMapping("/items/{id}")
   public ResponseEntity<Void> deleteItem(@PathVariable String id) {
     SearchCriteria deleteCriteria = SearchCriteria.builder().id(id).build();
     Set<MediaItem> items = library.search(deleteCriteria);
@@ -85,5 +80,4 @@ public class MediaItemsController {
     library.removeMediaItem(item, librarian);
     return ResponseEntity.noContent().build();
   }
-  
 }
