2d2fe212287fe04cfa1f4ee309e9008de7b828bd
brooklynharden <hardenbrooklyn123@gmail.com>
2025-10-09 22:12:09 +0000

    feat: Query return counts of media items, and Quesry returns sums of total pages checked out by guests

---
+++ lesson_25/db/db_app/src/main/resources/queries/brooklynharden/brooklynharden.sql
@@ -0,0 +1,15 @@
+-- database: ../../sqlite/data.db
+-- database: ../../sqlite/data.db
+SELECT
+COUNT(type)
+FROM
+media_items;
+
+
+SELECT guests.name, SUM(media_items.pages)
+FROM guests
+JOIN checked_out_items ON guests.email = checked_out_items.email
+INNER JOIN media_items ON checked_out_items.item_id = media_items.id
+GROUP BY 
+guests.name;
+
