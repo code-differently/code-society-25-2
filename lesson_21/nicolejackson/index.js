0b4b21be6edda1ba6330f20fae5b9884c9b0d62f
NcoleJac <ndjackson@gmail.com>
2025-10-09 00:34:15 +0000

    chore: making edits to index.js file

--- lesson_21/nicolejackson/index.js
+++ lesson_21/nicolejackson/index.js
@@ -1,18 +1,16 @@
-const express = require ("express")
-const app = express ()
-const port = 4000
-const cors = require ("cors") 4.4k (gzipped, 1.9k)
+const express = require("express");
+const app = express();
+const port = 4000;
+const cors = require("cors")
 
-app.use (express.urlencoded ({extended: true}))
-app.use (express.json())
-app.use (cors())
 
-app.get("/", cors() async (req, res)=> {
-    res.send("This is working")
+app.use(express.urlencoded ({extended: true})
+app.use(express.json())
+app.use(cors())
+
+ app.get("/", cors() async (req, res)) => {
+  res.send("This is Working!")
+ }
+ app.listen(port, () => {
+  console.log(`🚀 Server running at http://localhost:${port}`)
 })
-
-app.listen (port, ()=> {
-    console.log ("Listening at http//localhost: ${port}")
-
-})
-
