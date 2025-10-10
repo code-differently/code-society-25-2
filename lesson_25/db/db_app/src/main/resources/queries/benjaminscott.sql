3ff951291f98f09c59b6b516f87fd3ba1a0281f7
bscott519 <bbscott252@gmail.com>
2025-10-10 15:44:58 +0000

    completed SQL queries

---
+++ lesson_25/db/db_app/src/main/resources/queries/benjaminscott.sql
@@ -0,0 +1,13 @@
+SELECT type, COUNT(*)
+FROM media_items
+GROUP BY type;
+
+SELECT SUM(pages)
+FROM media_items
+JOIN checked_out_items ON media_items.id = checked_out_items.itemId;
+
+SELECT guests.name, guests.email, guests.type, checked_out_items.itemId, checked_out_items.dueDate
+FROM guests
+LEFT JOIN checked_out_items ON guests.email = checked_out_items.email;
+
+
