f9f85ba60d606269c614ff13f734afae6d6c01fc
Jared Edge <jarededgewrk@gmail.com>
2025-10-10 17:14:48 +0000

    feat: adds creates table and user loading

--- lesson_25/db/db_app/src/main/resources/queries/jarededge.sql
+++ lesson_25/db/db_app/src/main/resources/queries/jarededge.sql
@@ -1,4 +1,4 @@
-
+-- SQL Queries for Library Database
 SELECT type, COUNT(*) as count 
 FROM media_items 
 GROUP BY type;
@@ -11,3 +11,14 @@ SELECT guests.name, guests.email, media_items.title
 FROM guests 
 LEFT JOIN checked_out_items ON guests.email = checked_out_items.email
 LEFT JOIN media_items ON checked_out_items.item_id = media_items.id;
+
+-- Table Creation
+CREATE TABLE library_users (
+    id TEXT NOT NULL,
+    email TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (email)
+);
