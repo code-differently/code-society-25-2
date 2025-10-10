ef2c739bfa71e4b290b78a0e271e6ff49c6d2a96
Trinitie Jackson <txjackson252@gmail.com>
2025-10-10 03:23:09 +0000

    feat: adds unique file and SELECT queries

---
+++ lesson_25/db/db_app/src/main/resources/queries/trinitiejackson.sql
@@ -0,0 +1,12 @@
+SELECT type, COUNT(*) AS count
+FROM media_items
+GROUP BY type;
+
+SELECT SUM(pages) 
+FROM media_items
+JOIN checked_out_items ON media_items.id = checked_out_items.item_id;
+
+SELECT guests.name, guests.email, media_items.title
+FROM guests
+LEFT JOIN checked_out_items ON guests.email = checked_out_items.email
+LEFT JOIN media_items ON checked_out_items.item_id = media_items.id;
