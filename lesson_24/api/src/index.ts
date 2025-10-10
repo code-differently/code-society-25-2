70a74ec26121769d2fbb3cf9ddb47c7267fc0b15
calvin <cwrobinson252@mail.com>
2025-10-10 16:29:25 +0000

    fix the error adding program issue encounter due port conflicts

--- lesson_24/api/src/index.ts
+++ lesson_24/api/src/index.ts
@@ -1,9 +1,13 @@
 import {DbImpl} from './db.js';
 import {createServer} from './server.js';
 import path from 'path';
+import {fileURLToPath} from 'url';
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PROGRAMS_FILE = path.resolve(__dirname, './data/programs.json');
+
+console.log('Database file path:', PROGRAMS_FILE);
 
 const db = new DbImpl(PROGRAMS_FILE);
 createServer(db);
