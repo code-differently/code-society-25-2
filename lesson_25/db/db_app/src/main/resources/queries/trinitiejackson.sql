b039ec8f5f00e5b7ebf39511e24dde172a344313
Trinitie Jackson <txjackson252@gmail.com>
2025-10-10 03:36:18 +0000

    feat: adds library_users table

--- lesson_25/db/db_app/src/main/resources/queries/trinitiejackson.sql
+++ lesson_25/db/db_app/src/main/resources/queries/trinitiejackson.sql
@@ -10,3 +10,11 @@ SELECT guests.name, guests.email, media_items.title
 FROM guests
 LEFT JOIN checked_out_items ON guests.email = checked_out_items.email
 LEFT JOIN media_items ON checked_out_items.item_id = media_items.id;
+
+CREATE TABLE library_users (
+    id UUID PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
