0f7b049e746ea2d43049cf6cf6f1a0f0dea8868e
DaFDEe31 <adjocydanielson@gmail.com>
2025-10-10 04:46:47 +0000

    feat: creating library table

--- lesson_25/db/db_app/src/main/resources/queries/danielsonadjocy.sql
+++ lesson_25/db/db_app/src/main/resources/queries/danielsonadjocy.sql
@@ -10,3 +10,12 @@ where mediaItems.pages IS NOT NULL;
 select guests.id, guests.name, checked_out_items.item_id, checked_out_items.checked_out_date
 from guests
 left join checked_out_items on guests.id = checked_out_items.guest_id;
+
+CREATE Table library_users (
+    id UUID PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    password_hash CHAR(60) NOT NULL,
+    
+);
