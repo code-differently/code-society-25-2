c10e8417c81a09866f3288477678ea5c7d0b8b1d
Trinitie Jackson <txjackson252@gmail.com>
2025-10-10 05:00:55 +0000

    refactor: changes String to UUID

--- lesson_25/db/db_app/src/main/java/com/codedifferently/lesson25/repository/LibraryUserRepository.java
+++ lesson_25/db/db_app/src/main/java/com/codedifferently/lesson25/repository/LibraryUserRepository.java
@@ -2,9 +2,10 @@ package com.codedifferently.lesson25.repository;
 
 import com.codedifferently.lesson25.models.LibraryUserModel;
 import java.util.List;
+import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 
-public interface LibraryUserRepository extends CrudRepository<LibraryUserModel, String> {
+public interface LibraryUserRepository extends CrudRepository<LibraryUserModel, UUID> {
 
   @Override
   List<LibraryUserModel> findAll();
