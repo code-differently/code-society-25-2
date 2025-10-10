2fa6a5d1953dca07ad26ec892578b427aa3b89ff
Trinitie Jackson <txjackson252@gmail.com>
2025-10-10 05:21:02 +0000

    feat: adds insert values

--- lesson_25/db/db_app/src/main/resources/queries/trinitiejackson.sql
+++ lesson_25/db/db_app/src/main/resources/queries/trinitiejackson.sql
@@ -18,3 +18,7 @@ CREATE TABLE library_users (
     last_name TEXT NOT NULL,
     password_hash TEXT NOT NULL
 );
+
+INSERT INTO library_users (id, email, first_name, last_name, password_hash)
+VALUES ('550e8400-e29b-41d4-a716-446655440000', 'trinitie.jackson@example.com', 'Trinitie', 'Jackson', '$2y$10$fBjbX04w/CPZ4YZNJpLBR.hyuz1YXbyIFj92udVrckYEXjPzhoDHq'),
+       ('550e8400-e29b-41d4-a716-446655440001', 'john.doe@example.com', 'John', 'Doe', '$2y$10$QnMskk1wZ.T5148M9aALAuAeCHPRW72e3aN41Ysms4gOu1/YSjJn.');
