9881fac074dba39b3903ac51d4c508de828cc2f3
Talia Crockett <tmcrockett252@gmail.com>
2025-10-11 17:16:11 +0000

    feat: adds LibraryUserRepository

---
+++ lesson_25/db/db_app/src/main/java/com/codedifferently/lesson25/repository/LibraryUserRepository.java
@@ -0,0 +1,14 @@
+package com.codedifferently.lesson25.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.codedifferently.lesson25.models.LibraryUserModel;
+
+public interface LibraryUserRepository extends CrudRepository<LibraryUserModel, String> {
+   
+    @Override
+    List<LibraryUserModel> findAll();
+
+}
