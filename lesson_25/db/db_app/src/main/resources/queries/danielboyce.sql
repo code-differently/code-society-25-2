646a76440514dc2bd802dc6456c4656064b2d5fe
Daniel Boyce <danielboyce20@gmail.com>
2025-10-13 14:36:26 +0000

    feat:adds query to show all guests with checkout items

--- lesson_25/db/db_app/src/main/resources/queries/danielboyce.sql
+++ lesson_25/db/db_app/src/main/resources/queries/danielboyce.sql
@@ -1,11 +1,16 @@
-- database: ../../sqlite/data.db
+-- database: ../../sqlite/data.db
 SELECT COUNT(type) FROM media_items;
 
 
 SELECT SUM(pages) FROM 
-checkout_items
- INNER JOIN guests ON checkout_items.email = guest.email
- INNER JOIN media_items ON media_items.item_id = checkout_items.item_id
+checked_out_items
+ INNER JOIN guests ON  checked_out_items.email = guests.email
+ INNER JOIN media_items ON media_items.id = checked_out_items.item_id
  where pages is NOT NULL
 
 
+
+SELECT 
+guests.name,checked_out_items.*
+FROM 
+guests LEFT JOIN checked_out_items on guests.email = checked_out_items.email;
