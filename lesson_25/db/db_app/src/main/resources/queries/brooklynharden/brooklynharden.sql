bcdffaa4fda1e135a58124531d780d7bdaed7720
brooklynharden <hardenbrooklyn123@gmail.com>
2025-10-13 14:27:55 +0000

    refactor: edited sum select query

--- lesson_25/db/db_app/src/main/resources/queries/brooklynharden/brooklynharden.sql
+++ lesson_25/db/db_app/src/main/resources/queries/brooklynharden/brooklynharden.sql
@@ -1,17 +1,14 @@
 -- database: ../../sqlite/data.db
--- database: ../../sqlite/data.db
 SELECT
 COUNT(type)
 FROM
 media_items;
 
 
-SELECT guests.name, SUM(media_items.pages)
+SELECT SUM(media_items.pages)
 FROM guests
 JOIN checked_out_items ON guests.email = checked_out_items.email
-INNER JOIN media_items ON checked_out_items.item_id = media_items.id
-GROUP BY 
-guests.name;
+INNER JOIN media_items ON checked_out_items.item_id = media_items.id;
 
 
 SELECT guests.name, checked_out_items.*
