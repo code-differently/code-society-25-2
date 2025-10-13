1533fe452ca6f0c9a852a51464e57fd9197a9d86
brooklynharden <hardenbrooklyn123@gmail.com>
2025-10-13 13:32:32 +0000

    refactor: populate users into table, finish files/homework

---
+++ lesson_25/db/db_app/src/main/java/com/codedifferently/lesson25/repository/LibraryUserRepository.java
@@ -0,0 +1,6 @@
+package com.codedifferently.lesson25.repository;
+
+import com.codedifferently.lesson25.models.LibraryUserModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LibraryUserRepository extends CrudRepository<LibraryUserModel, String> {}
