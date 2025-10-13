afd0d4e1355bf8f690cc01e4490fa2ea8ef7a307
Daniel Boyce <danielboyce20@gmail.com>
2025-10-13 14:09:03 +0000

    feat:adds query to get total pages checked out by guests

--- lesson_25/db/db_app/src/main/resources/queries/danielboyce.sql
+++ lesson_25/db/db_app/src/main/resources/queries/danielboyce.sql
@@ -1,3 +1,11 @@
 - database: ../../sqlite/data.db
 SELECT COUNT(type) FROM media_items;
 
+
+SELECT SUM(pages) FROM 
+checkout_items
+ INNER JOIN guests ON checkout_items.email = guest.email
+ INNER JOIN media_items ON media_items.item_id = checkout_items.item_id
+ where pages is NOT NULL
+
+
